39e990d0be714c39ae26fe89f8f665cdb9f0552e
daniel-garcia <daniel@danielgarcia.info>
2013-08-20 14:34:41 -0500

    make example work.

--- serviced/examples/default.sh
+++ serviced/examples/default.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
 
+
+cd $GOPATH/src/github.com/zenoss/serviced/
 mysql -u root -e "drop database if exists cp; create database cp"
-mysql -u root cp -e "source ../database.sql"
+mysql -u root cp -e "source svc/database.sql"
 
-HOST=$(./serviced add-host localhost:4979)
-POOLID=$(./serviced add-pool default 0 0 0)
-
-# add host to pool
-./serviced add-host-to-pool $HOST $POOLID
+POOLID=$(./serviced/serviced add-pool default 0 0 0)
+HOST=$(./serviced/serviced add-host localhost:4979 $POOLID)
 
 COMMAND='/bin/sh -c "while true; do echo hello world; sleep 1; done"'
 
-SERVICE=$(./serviced add-service helloWorld $POOLID base $COMMAND)
+SERVICE=$(./serviced/serviced add-service helloWorld $POOLID base $COMMAND)
 
 echo "HOST = $HOST"
 echo "POOLID = $POOLID"
 echo "Hello, world service: $SERVICE"
 
-./serviced start-service $SERVICE
+./serviced/serviced start-service $SERVICE
 
 
 # create a hellohost service
 COMMAND='/helloHost'
-SERVICE=$(./serviced add-service hellHost $POOLID dgarcia/helloHost /helloHost)
+SERVICE=$(./serviced/serviced add-service hellHost $POOLID dgarcia/helloHost /helloHost)
 
-./serviced start-service $SERVICE
+./serviced/serviced start-service $SERVICE
 
 echo "hello host service: $SERVICE "
 
