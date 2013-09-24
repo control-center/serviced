553a4ba489b569a8281f63438ae4dcf4eba5bb64
Chad Schellenger <cschellenger@zenoss.com>
2013-09-24 18:00:15 -0500

    Improved documentation

--- web/static/js/controllers.js
+++ web/static/js/controllers.js
@@ -91,6 +91,7 @@ function EntryControl($scope, authService) {
     ];
 }
 
+// Used by /login view
 function LoginControl($scope, $http, $location, authService) {
     $scope.brand_label = "SERVICE DYNAMICS";
     $scope.login_button_text = "Log In";
@@ -103,15 +104,19 @@ function LoginControl($scope, $http, $location, authService) {
             }, 3000);
         });
     };
+    // Makes XHR POST with contents of login form
     $scope.login = function() {
         var creds = { "Username": $scope.username, "Password": $scope.password };
         $http.post('/login', creds).
             success(function(data, status) {
+                // Ensure that the auth service knows that we are logged in
                 authService.login(true);
-                console.log('Redirecting to /entry');
+                // Redirect to main page
                 $location.path('/entry');
             }).
             error(function(data, status) {
+                console.log('Login failed');
+                // Ensure that the auth service knows that the login failed
                 authService.login(false);
                 $scope.extra_class = 'has-error';
                 $scope.login_status = 'alert-danger';
@@ -122,6 +127,7 @@ function LoginControl($scope, $http, $location, authService) {
 }
 
 function WizardControl($scope, $location, wizardService, resourcesService, authService) {
+    // Ensure that if the user is not logged in, we show the /login page
     authService.checkLogin($scope);
 
     console.log('Initialized control for %s', $location.path());
@@ -132,7 +138,8 @@ function WizardControl($scope, $location, wizardService, resourcesService, authS
 
     // Ensure our scope has a list of pools
     refreshPools($scope, resourcesService, true);
-
+    
+    // The next function checks form validity then gets location from wizardService
     $scope.next = function(wizardForm) {
         $scope.nextClicked = true;
         if (wizardForm == null || wizardForm.$valid) {
@@ -143,18 +150,21 @@ function WizardControl($scope, $location, wizardService, resourcesService, authS
             console.log('Validation failed');
         }
     };
+    // All wizard pages have some kind of cancel function. Delegate location.
     $scope.cancel = function() {
         console.log('Cancel called from %s', $location.path());
         var nextPath = wizardService.cancel_page($location.path());
         $location.path(nextPath);
     };
 
+    // This call ensures that the previous page was processed.
     wizardService.fix_location($location);
 }
 
 
 // Controller for configuration
 function ConfigurationControl($scope, $routeParams, $location, servicesService, authService) {
+    // Ensure that if the user is not logged in, we show the /login page
     authService.checkLogin($scope);
 
     $scope.name = "configuration";
@@ -163,21 +173,27 @@ function ConfigurationControl($scope, $routeParams, $location, servicesService, 
         { label: 'Configuration', itemClass: 'active' }
     ];
 
+    // Build metadata necessary to display a table of services
     $scope.services = buildTable('Name', [
         { id: 'Name', name: 'Name'},
         { id: 'Description', name: 'Description'},
         { id: 'PoolId', name: 'Pool Id'},
         { id: 'Instances', name: 'Instances'}
     ]);
+
+    // Create a function for when the user clicks on a service
     $scope.click_service = function(serviceId) {
         var redirect = '/services/' + serviceId;
         $location.path(redirect);
     };
+
+    // Get the actual data about the services available
     refreshServices($scope, servicesService, false);
 }
 
 // Controller for configuration
 function ServiceControl($scope, $routeParams, servicesService, authService) {
+    // Ensure that if the user is not logged in, we show the /login page
     authService.checkLogin($scope);
 
     $scope.name = "configuration";
@@ -186,6 +202,8 @@ function ServiceControl($scope, $routeParams, servicesService, authService) {
         { label: 'Configuration', url: '#/services', itemClass: '' },
         { label: $scope.params.serviceId, itemClass: 'active' }
     ];
+
+    // Create metadata necessary to display service details
     $scope.services = {
         headers: [
             { name: 'Id', id: 'Id' },
@@ -197,6 +215,8 @@ function ServiceControl($scope, $routeParams, servicesService, authService) {
             { name: 'Desired State', id: 'DesiredState' }
         ]
     };
+
+    // Ensure that we have service data populated
     refreshServices($scope, servicesService, true);
 }
 
@@ -204,19 +224,27 @@ function ServiceControl($scope, $routeParams, servicesService, authService) {
 function ActionControl($scope, $routeParams, $location, resourcesService, servicesService) {
     $scope.name = 'actions';
     $scope.params = $routeParams;
+
+    // New hosts should belong to the current pool by default
     $scope.newHost = {
         PoolId: $scope.params.poolId
     };
+
+    // New pools should belong to the current pool by default
     $scope.newPool = {
         ParentId: $scope.params.poolId
     };
+
+    // Just create a stub for new services
     $scope.newService = {};
 
+    // Function for adding new hosts
     $scope.add_host = function() {
         console.log('Adding host %s as child of pool %s', 
                     $scope.newHost.Name, $scope.newHost.PoolId);
 
         resourcesService.add_host($scope.newHost, function(data) {
+            // After adding, refresh our list
             refreshHosts($scope, resourcesService, false);
         });
         // Reset for another add
@@ -225,9 +253,11 @@ function ActionControl($scope, $routeParams, $location, resourcesService, servic
         };
     };
 
+    // Function for adding new pools
     $scope.add_pool = function() {
         console.log('Adding pool %s as child of pool %s', $scope.newPool.Id, $scope.params.poolId);
         resourcesService.add_pool($scope.newPool, function(data) {
+            // After adding, refresh our list
             refreshPools($scope, resourcesService, false);
         });
         // Reset for another add
@@ -236,68 +266,96 @@ function ActionControl($scope, $routeParams, $location, resourcesService, servic
         };
     };
 
+    // Function for removing the current pool
     $scope.remove_pool = function() {
         console.log('Removing pool %s', $scope.params.poolId);
         resourcesService.remove_pool($scope.params.poolId, function(data) {
-            var redirect = '/resources';
+
+            // The UI can be weird if we don't wait for the modal to hide
+            // before we change the path
             $('#removePool').on('hidden.bs.modal', function() {
+                var redirect = '/resources';
                 console.log('Redirecting to %s', redirect);
                 $location.path(redirect);
+
+                // Because this is happening in a weird place, the $scope 
+                // seems to need a hint
                 $scope.$apply();
             });
 
         });
     };
 
+    // Function for removing the current host
     $scope.remove_host = function() {
         console.log('Removing host %s', $scope.params.hostId);
         resourcesService.remove_host($scope.params.hostId, function(data) {
-            var redirect = '/pools/' + $scope.params.poolId;
+
+            // The UI can be weird if we don't wait for the modal to hide
+            // before we change the path
             $('#removeHost').on('hidden.bs.modal', function() {
+                var redirect = '/pools/' + $scope.params.poolId;
                 console.log('Redirecting to %s', redirect);
                 $location.path(redirect);
+
+                // Because this is happening in a weird place, the $scope 
+                // seems to need a hint
                 $scope.$apply();
             });
         });
     };
 
+    // Function for editing the current pool
     $scope.edit_pool = function() {
         console.log('Updating pool %s', $scope.params.poolId);
         resourcesService.update_pool($scope.params.poolId, $scope.pools.current, function(data) {
+            // After the edit succeeds, refresh the list
             refreshPools($scope, resourcesService, false);
         });
     };
 
+    // Function for editing the current host
     $scope.edit_host = function() {
         console.log('Updating host %s', $scope.params.hostId);
         resourcesService.update_host($scope.params.hostId, $scope.hosts.current, function(data) {
+            // After the edit succeeds, refresh the list
             refreshHosts($scope, resourcesService, false, false);
         });
     };
 
+    // Function for adding a new host
     $scope.add_service = function() {
         console.log('Adding service %s', $scope.newService.Name);
-
         servicesService.add_service($scope.newService, function(data) {
+            // After the add succeeds, refresh the list
             refreshServices($scope, servicesService, false);
         });
         // Reset for another add
         $scope.newService = {};
     };
 
+    // Function for editing the current service
     $scope.edit_service = function() {
         console.log('Editing service %s', $scope.services.current.Name);
         servicesService.update_service($scope.params.serviceId, $scope.services.current, function(data) {
+            // After the edit succeeds, refresh the list
             refreshServices($scope, servicesService, false);
         });
     };
 
+    // Function for removing the current service
     $scope.remove_service = function() {
         console.log('Removing service %s', $scope.params.serviceId);
         servicesService.remove_service($scope.params.serviceId, function(data) {
+
+            // The UI can be weird if we don't wait for the modal to hide
+            // before we change the path
             $('#removeService').on('hidden.bs.modal', function() {
                 console.log('redirecting to /services');
                 $location.path('/services');
+
+                // Because this is happening in a weird place, the $scope 
+                // seems to need a hint
                 $scope.$apply();
             });
         });
@@ -307,6 +365,7 @@ function ActionControl($scope, $routeParams, $location, resourcesService, servic
 
 // Controller for resources
 function ResourcesControl($scope, $routeParams, $location, resourcesService, authService) {
+    // Ensure logged in
     authService.checkLogin($scope);
 
     $scope.name = "resources";
@@ -314,23 +373,29 @@ function ResourcesControl($scope, $routeParams, $location, resourcesService, aut
     $scope.breadcrumbs = [
         { label: 'Resources', itemClass: 'active' }
     ];
+    // Build metadata for displaying a list of pools
     $scope.pools = buildTable('Id', [
         { id: 'Id', name: 'Id'}, 
         { id: 'ParentId', name: 'Parent Id'},
         { id: 'Priority', name: 'Priority'}
     ]);
+
+    // Create function for selecting a pool
     $scope.click_pool = function(poolId) {
         var redirect = '/pools/' + poolId;
         $location.path(redirect);
     }
     $scope.hosts = {};
 
+    // Ensure we have a list of pools
     refreshPools($scope, resourcesService, false);
+    // Also ensure we have a list of hosts
     refreshHosts($scope, resourcesService, false, false);
 }
 
 // Controller for resources -> pool details
 function PoolControl($scope, $routeParams, $http, $location, resourcesService, authService) {
+    // Ensure logged in
     authService.checkLogin($scope);
 
     $scope.name = "pool-details";
@@ -340,6 +405,7 @@ function PoolControl($scope, $routeParams, $http, $location, resourcesService, a
         { label: $scope.params.poolId, itemClass: 'active' }
     ];
 
+    // Build metadata for displaying pool details
     $scope.pools = {
         headers: [
             { id: 'Id', name: 'Id'}, 
@@ -349,22 +415,30 @@ function PoolControl($scope, $routeParams, $http, $location, resourcesService, a
             { id: 'Priority', name: 'Priority'}
         ]
     };
+    // Populate list of pools
     refreshPools($scope, resourcesService, true);
+
+    // Create function for selecting a host
     $scope.click_host = function(pool, host) {
         var redirect = '/pools/' + pool + "/hosts/" + host;
         console.log('Redirecting to %s', redirect);
         $location.path(redirect);
     };
+
+    // Build metadata for displaying a list of hosts
     $scope.hosts = buildTable('Name', [
         { id: 'Name', name: 'Name'},
         { id: 'IpAddr', name: 'IP Address'},
         { id: 'PrivateNetwork', name: 'Private Network'}
     ]);
+
+    // Populate list of hosts
     refreshHosts($scope, resourcesService, true, false);
 }
 
 // Controller for resources -> pool details -> host details
 function HostControl($scope, $routeParams, $http, resourcesService, authService) {
+    // Ensure logged in
     authService.checkLogin($scope);
 
     $scope.name = "host-details"
@@ -376,7 +450,8 @@ function HostControl($scope, $routeParams, $http, resourcesService, authService)
     ];
 
     $scope.pools = {};
-    refreshPools($scope, resourcesService, true);
+
+    // Build metadata for displaying host details
     $scope.hosts = {
         headers: [
             { id: 'Id', name: 'Id'}, 
@@ -387,7 +462,11 @@ function HostControl($scope, $routeParams, $http, resourcesService, authService)
             { id: 'PrivateNetwork', name: 'Private Network'}
         ]
     };
-    console.log('In scope for host ' + $scope.params.hostId);
+
+    // Populate list of pools
+    refreshPools($scope, resourcesService, true);
+
+    // Populate list of hosts
     refreshHosts($scope, resourcesService, true, true);
 }
 
@@ -403,13 +482,19 @@ function NavbarControl($scope, $http, $cookies, $location, authService) {
         { url: '#/services', label: 'Configuration' },
         { url: '#/resources', label: 'Resources' }
     ];
+
+    // Create a logout function
     $scope.logout = function() {
+        // Set internal state to logged out.
         authService.login(false);
+        // Make http call to logout from server
         $http.delete('/login').
             success(function(data, status) {
+                // On successful logout, redirect to /login
                 $location.path('/login');
             }).
             error(function(data, status) {
+                // On failure to logout, note the error
                 console.log('Unable to log out. Were you logged in to begin with?');
             });
     };
@@ -420,25 +505,45 @@ function NavbarControl($scope, $http, $cookies, $location, authService) {
  ******************************************************************************/
 
 function WizardService() {
+    // Wizard data is a context object that persists across controllers, but not
+    // across full page loads. This is fine for what we want since we are 
+    // controlling the view without full page loads.
     var wizard_data;
+
+    // This is a lazy-loading accessor function that defines a default context
+    // if one does not yet exist.
     var _get_wizard_data = function() {
         if (wizard_data === undefined) {
             wizard_data = {
+                // Temporary: local or distributed
                 installType: 'local',
+
+                // Default install type
                 localInstallType: 'Resource Manager',
+
+                // List of products to install
                 installOptions: [
                     'Resource Manager',
                     'Impact',
                     'Analytics'
                 ],
+
+                // Default pool
                 destination: 'default',
+
+                // The 'flow' field defines the order of the pages
                 flow: [
                     '/wizard/start', 
                     '/wizard/page1', 
                     '/wizard/page2', 
                     '/wizard/finish'
                 ],
+
+                // Where to go when a user cancels the flow
                 cancel: '/',
+
+                // The 'done' field defines which pages have successfully been
+                // completed.
                 done: {}
             };
         }
@@ -462,11 +567,16 @@ function WizardService() {
             }
         }
         
+        // Assume we don't need to redirect
         var needToRedirect = false;
+
         // We only redirect if this page is in the flow and the requirements are
         // not met.
         while (pageIndex > 0 && d.done[d.flow[pageIndex -1]] !== true) {
+            // The requirements are not met for the current page. Set the 
+            // current page to the previous page and check again.
             pageIndex -= 1;
+            // We definitely need to redirect.
             needToRedirect = true;
         }
 
@@ -474,21 +584,22 @@ function WizardService() {
             var redirect = d.flow[pageIndex];
             console.log('Requirements not met so redirecting to: %s', redirect);
             $location.path(redirect);
-        } else {
-            console.log('Requirements met for page %s', $location.path());
         }
     };
 
+    // This function is used to mark the current page as complete.
     var _current_done = function(currentPath) {
         var d = _get_wizard_data();
         d.done[currentPath] = true;
     };
 
+    // This function is used to return the destination when a user cancels
     var _cancel_page = function(currentPath) {
         var d = _get_wizard_data();
         return d.cancel;
     };
 
+    // Mark the current page as complete then return the path for the next page
     var _next_page = function(currentPath) {
         _current_done(currentPath);
         var d = _get_wizard_data();
@@ -503,6 +614,7 @@ function WizardService() {
         return d.flow[pageIndex];
     };
 
+    // This function is a factory. Return an object.
     return {
         get_context: _get_wizard_data,
         fix_location: _fix_location,
