8c591c8264eae48cac796c1bc5d4a4a3fc2a5991
Ian McCracken <ian.mccracken@gmail.com>
2013-10-31 10:30:02 -0500

    Changes

--- serviced/proxy.go
+++ serviced/proxy.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"github.com/zenoss/glog"
-	sproxy "github.com/zenoss/serviced/proxy"
 	"github.com/zenoss/serviced"
+	sproxy "github.com/zenoss/serviced/proxy"
 
 	"fmt"
-	"io"
 	"net"
 	"os"
 	"os/exec"
@@ -41,18 +40,10 @@ func (cli *ServicedCli) CmdProxy(args ...string) error {
 		for {
 			glog.Infof("About to execute: %s", cmdString)
 			cmd := exec.Command("bash", "-c", cmdString)
-			stderr, err := cmd.StderrPipe()
-			if err != nil {
-				glog.Fatalf("Problem opening a stderr pipe to service: %s", err)
-			}
-			go io.Copy(os.Stderr, stderr)
-			stdout, err := cmd.StdoutPipe()
-			if err != nil {
-				glog.Fatalf("Problem opening a stdout pipe to service: %s", err)
-			}
-			go io.Copy(os.Stdout, stdout)
-			glog.Infof("About to execute: %s", cmdString)
-			err = cmd.Run()
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Stdin = os.Stdin
+			err := cmd.Run()
 			if err != nil {
 				glog.Errorf("Problem running service: %v", err)
 				glog.Flush()
@@ -63,61 +54,61 @@ func (cli *ServicedCli) CmdProxy(args ...string) error {
 	}(config.Command)
 
 	for {
-	func() {
-		client, err := sproxy.NewLBClient(proxyOptions.servicedEndpoint)
-		if err != nil {
-			glog.Errorf("Could not create a client to endpoint %s: %s", proxyOptions.servicedEndpoint, err)
-			return
-		}
-		defer client.Close()
-
-		var endpoints map[string][]*serviced.ApplicationEndpoint
-		err = client.GetServiceEndpoints(config.ServiceId, &endpoints)
-		if err != nil {
-			glog.Errorf("Error getting application endpoints for service %s: %s", config.ServiceId, err)
-			return
-		}
-
-		for key, endpointList := range endpoints {
-			if len(endpointList) <= 0 {
-				glog.Warningf("No endpoints found for %s", key)
-				if proxy, ok := proxies[key]; ok {
-					emptyAddressList := make([]string, 0)
-					proxy.SetNewAddresses(emptyAddressList)
-				}
-				continue
+		func() {
+			client, err := sproxy.NewLBClient(proxyOptions.servicedEndpoint)
+			if err != nil {
+				glog.Errorf("Could not create a client to endpoint %s: %s", proxyOptions.servicedEndpoint, err)
+				return
 			}
-			addresses := make([]string, len(endpointList))
-			for i, endpoint := range endpointList {
-				addresses[i] = fmt.Sprintf("%s:%d", endpoint.HostIp, endpoint.HostPort)
-			}
-			sort.Strings(addresses)
+			defer client.Close()
 
-			var proxy *sproxy.Proxy
-			var ok bool
-			if proxy, ok = proxies[key]; !ok {
-				// setup a new proxy
-				listener, err := net.Listen("tcp4", fmt.Sprintf(":%d", endpointList[0].ContainerPort))
-				if err != nil {
-					glog.Errorf("Could not bind to port: %s", err)
+			var endpoints map[string][]*serviced.ApplicationEndpoint
+			err = client.GetServiceEndpoints(config.ServiceId, &endpoints)
+			if err != nil {
+				glog.Errorf("Error getting application endpoints for service %s: %s", config.ServiceId, err)
+				return
+			}
+
+			for key, endpointList := range endpoints {
+				if len(endpointList) <= 0 {
+					glog.Warningf("No endpoints found for %s", key)
+					if proxy, ok := proxies[key]; ok {
+						emptyAddressList := make([]string, 0)
+						proxy.SetNewAddresses(emptyAddressList)
+					}
 					continue
 				}
-				proxy, err = sproxy.NewProxy(
-					fmt.Sprintf("%v", endpointList[0]),
-					uint16(config.TCPMux.Port),
-					config.TCPMux.UseTLS,
-					listener)
-				if err != nil {
-					glog.Errorf("Could not build proxy %s", err)
-					continue
+				addresses := make([]string, len(endpointList))
+				for i, endpoint := range endpointList {
+					addresses[i] = fmt.Sprintf("%s:%d", endpoint.HostIp, endpoint.HostPort)
 				}
-				proxies[key] = proxy
-			}
-			proxy.SetNewAddresses(addresses)
-		}
-	}()
+				sort.Strings(addresses)
 
-		time.Sleep(time.Second*10)
+				var proxy *sproxy.Proxy
+				var ok bool
+				if proxy, ok = proxies[key]; !ok {
+					// setup a new proxy
+					listener, err := net.Listen("tcp4", fmt.Sprintf(":%d", endpointList[0].ContainerPort))
+					if err != nil {
+						glog.Errorf("Could not bind to port: %s", err)
+						continue
+					}
+					proxy, err = sproxy.NewProxy(
+						fmt.Sprintf("%v", endpointList[0]),
+						uint16(config.TCPMux.Port),
+						config.TCPMux.UseTLS,
+						listener)
+					if err != nil {
+						glog.Errorf("Could not build proxy %s", err)
+						continue
+					}
+					proxies[key] = proxy
+				}
+				proxy.SetNewAddresses(addresses)
+			}
+		}()
+
+		time.Sleep(time.Second * 10)
 	}
 
 	glog.Flush()
@@ -125,10 +116,8 @@ func (cli *ServicedCli) CmdProxy(args ...string) error {
 	return nil
 }
 
-
-
-var proxies map[string] *sproxy.Proxy
+var proxies map[string]*sproxy.Proxy
 
 func init() {
-	proxies = make(map[string] *sproxy.Proxy)
+	proxies = make(map[string]*sproxy.Proxy)
 }
