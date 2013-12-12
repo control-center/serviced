6f5571f79445905ed19aa47eea2d4ccb1a3e970f
daniel-garcia <daniel@danielgarcia.info>
2013-12-12 14:26:24 -0600

    improve opentsdb startup

---
+++ repos/opentsdb/start-opentsdb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p /tmp/tsd
+
+echo "Starting HBase..."
+/opt/hbase-0.94.14/bin/start-hbase.sh 
+
+while [[ `/opt/opentsdb/src/create_table.sh` != *"ERROR: Table already exists: tsdb"* ]]; do
+    echo `date` ": Waiting for HBase to be ready..."
+    sleep 2
+done
+
+echo "Starting opentsdb..."
+exec /opt/opentsdb/build/tsdb tsd --port=4242 --staticroot=/opt/opentsdb/build/staticroot --cachedir=/tmp/tsd
+
