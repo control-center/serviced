b562a5fae70747fede9c4680394ae88c21d4f2bc
Seth Cleveland <seth.cleveland@gmail.com>
2013-12-13 16:03:20 -0600

    Migrated agent injectContext tests to service_tests Added test case for injecting service keys/fields

--- dao/tests/service_test.go
+++ dao/tests/service_test.go
@@ -58,20 +58,36 @@ var testcases = []struct {
 		ParentServiceId: "1",
 		CreatedAt:       time.Now(),
 		UpdatedAt:       time.Now(),
-	}, "/usr/bin/ping -c 32 zenoss.com --monitor monitor"},
+	}, "/usr/bin/ping -c 32 a_hostname"},
+	{dao.Service{
+		Id:              "3",
+		Name:            "/bin/sh",
+		Context:         "",
+		Startup:         "{{.Name}} ls -l .",
+		Description:     "Execute ls -l on .",
+		Instances:       1,
+		ImageId:         "test/bin_sh",
+		PoolId:          "default",
+		DesiredState:    1,
+		Launch:          "auto",
+		Endpoints:       []dao.ServiceEndpoint{},
+		ParentServiceId: "1",
+		CreatedAt:       time.Now(),
+		UpdatedAt:       time.Now(),
+	}, "/bin/sh ls -l ."},
 }
 
 var addresses []string
 var cp, err = elasticsearch.NewControlSvc("localhost", 9200, addresses)
 
 func init() {
-  var unused int
+	var unused int
 	if err == nil {
 		err = isvcs.ElasticSearchContainer.Run()
 		if err == nil {
 			for _, testcase := range testcases {
 				var id string
-        cp.RemoveService( testcase.service.Id, &unused)
+				cp.RemoveService(testcase.service.Id, &unused)
 				if err = cp.AddService(testcase.service, &id); err != nil {
 					glog.Fatalf("Failed Loading Service: %+v, %s", testcase.service, err)
 				}
@@ -84,7 +100,37 @@ func init() {
 
 func TestEvaluateContext(t *testing.T) {
 	for _, testcase := range testcases {
+		glog.Infof("Service.Startup before: %s, %s", testcase.service.Startup)
 		err = testcase.service.EvaluateContext(cp)
-		glog.Infof("Service: %+v, %s", testcase.service, err)
+		glog.Infof("Service.Startup after: %s, %s", testcase.service.Startup, err)
+
+		result := testcase.service.Startup
+		if result != testcase.expected {
+			t.Errorf("Expecting \"%s\" got \"%s\"\n", testcase.expected, result)
+		}
+	}
+}
+
+func TestIncompleteInjection(t *testing.T) {
+	service := dao.Service{
+		Id:              "0",
+		Name:            "pinger",
+		Context:         "{\"RemoteHost\": \"zenoss.com\"}",
+		Startup:         "/usr/bin/ping -c {{(context .).Count}} {{(context .).RemoteHost}}",
+		Description:     "Ping a remote host a fixed number of times",
+		Instances:       1,
+		ImageId:         "test/pinger",
+		PoolId:          "default",
+		DesiredState:    1,
+		Launch:          "auto",
+		Endpoints:       []dao.ServiceEndpoint{},
+		ParentServiceId: "0987654321",
+		CreatedAt:       time.Now(),
+		UpdatedAt:       time.Now(),
+	}
+
+	service.EvaluateContext(cp)
+	if service.Startup == "/usr/bin/ping -c 64 zenoss.com" {
+		t.Errorf("Not expecting a match")
 	}
 }
