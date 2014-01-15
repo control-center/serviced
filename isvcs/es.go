aef528a88747ec2b5ef317378b37dce25f63e863
daniel-garcia <daniel@danielgarcia.info>
2014-01-15 16:53:06 -0600

    fix race condition on startup

--- isvcs/es.go
+++ isvcs/es.go
@@ -1,10 +1,11 @@
 package isvcs
 
 import (
-	"github.com/zenoss/glog"
-
 	"fmt"
+	"github.com/mattbaird/elastigo/cluster"
+	"github.com/zenoss/glog"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -31,16 +32,24 @@ func (c *ElasticSearchISvc) Run() error {
 
 	start := time.Now()
 	timeout := time.Second * 30
+
+	schemaFile := localDir("resources/controlplane.json")
+
 	for {
-		if _, err := http.Get("http://localhost:9200/"); err == nil {
+		if healthResponse, err := cluster.Health(true); err == nil && (healthResponse.Status == "green" || healthResponse.Status == "yellow") {
+			if buffer, err := os.Open(schemaFile); err != nil {
+				glog.Fatalf("problem reading %s", err)
+			} else {
+				http.Post("http://localhost:9200/controlplane", "application/json", buffer)
+			}
 			break
 		} else {
-			glog.V(2).Infof("Still trying to connect to elastic: %v", err)
+			glog.V(2).Infof("Still trying to connect to elastic: %v: %s", err, healthResponse)
 		}
 		if time.Since(start) > timeout && time.Since(start) < (timeout/4) {
 			return fmt.Errorf("Could not startup elastic search container.")
 		}
-		time.Sleep(time.Millisecond * 100)
+		time.Sleep(time.Millisecond * 1000)
 	}
 	return nil
 }
