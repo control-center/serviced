ef9c12e325635a57e168cf809e16648c6fcb77c6
Ian McCracken <ian.mccracken@gmail.com>
2014-01-17 13:33:25 -0600

    Forgot port 8888 for query

--- isvcs/opentsdb.go
+++ isvcs/opentsdb.go
@@ -20,7 +20,7 @@ func init() {
 			"zctrl/isvcs",
 			"v1",
 			`/bin/bash -c "cd /opt/zenoss && supervisord -n -c /opt/zenoss/etc/supervisor.conf"`,
-			[]int{4242, 8443, 9090, 60000, 60010, 60020, 60030},
+			[]int{4242, 8443, 8888, 9090, 60000, 60010, 60020, 60030},
 			[]string{"/opt/zenoss/var/hbase"},
 		),
 	}
