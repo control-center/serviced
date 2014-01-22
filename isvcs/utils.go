655d83df9e0dfb0791ca17e13ade11a2c321f0be
daniel-garcia <daniel@danielgarcia.info>
2014-01-22 13:02:50 -0600

    update copyright license

--- isvcs/utils.go
+++ isvcs/utils.go
@@ -1,14 +1,20 @@
 /*******************************************************************************
-* Copyright (C) Zenoss, Inc. 2014 all rights reserved.
+* Copyright (C) Zenoss, Inc. 2013, 2014, all rights reserved.
 *
 * This content is made available according to terms specified in
 * License.zenoss under the directory where your Zenoss product is installed.
 *
 *******************************************************************************/
 
+package isvcs
+
+import (
+	"os"
+)
+
 var randomSource string
 
-func() init{
+func init() {
 	randomSource = "/dev/urandom"
 }
 
