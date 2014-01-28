3a3f9fba55e1771771c0ffcc62fdb71e1c6c8ded
Daniel Garcia <daniel@danielgarcia.info>
2014-01-28 11:18:51 -0600

    update copyright to be idiomatic

--- circular/buffer_test.go
+++ circular/buffer_test.go
@@ -1,6 +1,6 @@
-/*******************************************************************************
-* Copyright (C) Zenoss, Inc. 2014, all rights reserved.
-*******************************************************************************/
+// Copyright 2014, The Serviced Authors. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
 
 package circular
 
