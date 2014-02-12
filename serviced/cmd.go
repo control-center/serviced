7abe53e9dc9e52348804bd2747c1d2cfc1e957a8
Summer Mousa <smousa@zenoss.com>
2014-02-12 16:19:39 -0600

    unhardcoded the browser terminal

--- serviced/cmd.go
+++ serviced/cmd.go
@@ -199,7 +199,11 @@ func startServer() {
 		// Currently its only use is for command execution.
 		go func() {
 			sio := shell.NewProcessExecutorServer(options.port)
-			sio.Handle("/", http.FileServer(http.Dir("/home/zenoss/europa/src/golang/src/github.com/zenoss/serviced/serviced/www/")))
+			dir, _, err := serviced.ExecPath()
+			if err != nil {
+				glog.Fatalf("could not find path to serviced, %v", err)
+			}
+			sio.Handle("/", http.FileServer(http.Dir(path.Join(dir, "www"))))
 			http.ListenAndServe(":50000", sio)
 		}()
 	}
