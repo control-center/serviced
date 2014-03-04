6d2f7c460fa2b55dddd87cdea9dd093049a81abe
Ian McCracken <ian.mccracken@gmail.com>
2014-03-04 10:26:54 -0600

    Get the loading working

--- dao/template.go
+++ dao/template.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -29,8 +28,46 @@ func ServiceDefinitionFromPath(path string) (*ServiceDefinition, error) {
 	return getServiceDefinition(path)
 }
 
+// Is this the best way to do a set?
+var fileswecareabout = map[string]struct{}{
+	"service.json": struct{}{},
+	"-CONFIGS-":    struct{}{},
+	"FILTERS":      struct{}{},
+}
+
 func serviceJSONFilter(filename string) bool {
-	return strings.HasSuffix(filename, "/service.json")
+	_, ok := fileswecareabout[filepath.Base(filename)]
+	return ok
+}
+
+// pathContains tells you whether s is a segment of path.
+func pathContains(s, path string) bool {
+	for len(path) >= 0 {
+		dir, file := filepath.Split(path)
+		if file == s {
+			return true
+		}
+		path = strings.TrimSuffix(dir, "/")
+	}
+	return false
+}
+
+// pathSplit splits a path on a given segment, returning the parent and child
+// paths with separators trimmed appropriately.
+//
+// Example:
+//     fmt.Println(pathSplit("/a/b/c/d", "c"))
+// Result:
+//    /a/b d
+//
+func pathSplit(path, segment string) (string, string) {
+	split := strings.Split(path, segment)
+	parent := split[0]
+	child := ""
+	if len(split) > 1 {
+		child = split[1]
+	}
+	return strings.TrimSuffix(parent, "/"), strings.TrimSuffix(child, "/")
 }
 
 func getServiceDefinition(path string) (serviceDef *ServiceDefinition, err error) {
@@ -39,110 +76,131 @@ func getServiceDefinition(path string) (serviceDef *ServiceDefinition, err error
 	if err != nil {
 		return nil, err
 	}
-	for files.Iterate(serviceJSONFilter) {
-		// TODO: stuff
-		_, err := ioutil.ReadAll(files)
-		if err != nil {
-			return nil, err
-		}
-	}
-	// TODO: Replace the rest!
 
-	// is path a dir
-	fi, err := os.Stat(path)
-	if err != nil {
-		return nil, err
-	}
-	if !fi.IsDir() {
-		return nil, fmt.Errorf("Given path is not a directory")
-	}
+	defs := map[string]*ServiceDefinition{}
+	confs := map[string][]ConfigFile{}
+	filters := map[string]map[string]string{}
 
-	// look for service.json
-	serviceFile := fmt.Sprintf("%s/service.json", path)
-	blob, err := ioutil.ReadFile(serviceFile)
-	if err != nil {
-		return nil, err
-	}
-
-	// load blob
-	svc := ServiceDefinition{}
-	err = json.Unmarshal(blob, &svc)
-	if err != nil {
-		glog.Errorf("Could not unmarshal service at %s", path)
-		return nil, err
-	}
-	svc.Name = filepath.Base(path)
-	if svc.ConfigFiles == nil {
-		svc.ConfigFiles = make(map[string]ConfigFile)
-	}
-
-	// look at sub services
-	subServices := make(map[string]*ServiceDefinition)
-	subpaths, err := ioutil.ReadDir(path)
-	if err != nil {
-		return nil, err
-	}
-	for _, subpath := range subpaths {
+	// Create necessary objects from the reader
+	for files.Iterate(nil) {
+		name := files.Name()
+		parent, base := filepath.Dir(name), filepath.Base(name)
 		switch {
-		case subpath.Name() == "service.json":
-			continue
-		case subpath.Name() == "makefile": // ignoring makefiles present in service defs
-			continue
-		case subpath.Name() == "-CONFIGS-":
-			if !subpath.IsDir() {
-				return nil, fmt.Errorf("-CONFIGS- must be a director: %s", path)
+		case base == "service.json":
+			// load blob
+			svc := ServiceDefinition{}
+			err = json.NewDecoder(files).Decode(&svc)
+			if err != nil {
+				glog.Errorf("Could not unmarshal service at %s", path)
+				return nil, err
 			}
-			getFiles := func(p string, f os.FileInfo, err error) error {
-				if f.IsDir() {
-					return nil
-				}
-				buffer, err := ioutil.ReadFile(p)
-				if err != nil {
-					return err
-				}
-				path := p[len(path)+len(subpath.Name())+1:]
-				if _, ok := svc.ConfigFiles[path]; !ok {
-					svc.ConfigFiles[path] = ConfigFile{
-						Filename: path,
-						Content:  string(buffer),
-					}
-				} else {
-					configFile := svc.ConfigFiles[path]
-					configFile.Content = string(buffer)
-				}
-				return nil
+			svc.Name = filepath.Base(path)
+			if svc.ConfigFiles == nil {
+				svc.ConfigFiles = make(map[string]ConfigFile)
 			}
-			err = filepath.Walk(path+"/"+subpath.Name(), getFiles)
+			defs[parent] = &svc
+		case pathContains("-CONFIGS-", name):
+			buffer, err := ioutil.ReadAll(files)
 			if err != nil {
 				return nil, err
 			}
-		case subpath.Name() == "FILTERS":
-			if !subpath.IsDir() {
-				return nil, fmt.Errorf(path + "/-FILTERS- must be a directory.")
+			p, child := pathSplit(name, "-CONFIGS-")
+			if _, ok := confs[p]; !ok {
+				confs[p] = []ConfigFile{}
 			}
-			filters, err := getFiltersFromDirectory(path + "/" + subpath.Name())
+			confs[p] = append(confs[p], ConfigFile{
+				Filename: "/" + child,
+				Content:  string(buffer),
+			})
+		case strings.Contains(name, "FILTERS"):
+			// make sure it is a valid filter
+			if !strings.HasSuffix(base, ".conf") {
+				glog.Warning("Skipping %s because it doesn't have a .conf extension", base)
+				continue
+			}
+			contents, err := ioutil.ReadAll(files)
 			if err != nil {
-				glog.Errorf("Error fetching filters at "+path, err)
-			} else {
-				svc.LogFilters = filters
+				glog.Errorf("Unable to read the file %s, skipping", name)
+				continue
 			}
-		case subpath.IsDir():
-			subsvc, err := getServiceDefinition(path + "/" + subpath.Name())
-			if err != nil {
-				return nil, err
+			base = strings.TrimSuffix(base, ".conf")
+			if _, ok := filters[parent]; !ok {
+				filters[parent] = map[string]string{}
 			}
-			subServices[subpath.Name()] = subsvc
-		default:
-			glog.Errorf("Unreconised file %s at %s", subpath, path)
+			filters[parent][base] = string(contents)
 		}
 	}
-	svc.Services = make([]ServiceDefinition, len(subServices))
-	i := 0
-	for _, subsvc := range subServices {
-		svc.Services[i] = *subsvc
-		i += 1
-	}
-	return &svc, err
+	fmt.Println(defs)
+	// Now stitch it together
+	return &ServiceDefinition{}, nil
+
+	//// look at sub services
+	//subServices := make(map[string]*ServiceDefinition)
+	//subpaths, err := ioutil.ReadDir(path)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//for _, subpath := range subpaths {
+	//	switch {
+	//	case subpath.Name() == "service.json":
+	//		continue
+	//	case subpath.Name() == "makefile": // ignoring makefiles present in service defs
+	//		continue
+	//	case subpath.Name() == "-CONFIGS-":
+	//		if !subpath.IsDir() {
+	//			return nil, fmt.Errorf("-CONFIGS- must be a director: %s", path)
+	//		}
+	//		getFiles := func(p string, f os.FileInfo, err error) error {
+	//			if f.IsDir() {
+	//				return nil
+	//			}
+	//			buffer, err := ioutil.ReadFile(p)
+	//			if err != nil {
+	//				return err
+	//			}
+	//			path := p[len(path)+len(subpath.Name())+1:]
+	//			if _, ok := svc.ConfigFiles[path]; !ok {
+	//				svc.ConfigFiles[path] = ConfigFile{
+	//					Filename: path,
+	//					Content:  string(buffer),
+	//				}
+	//			} else {
+	//				configFile := svc.ConfigFiles[path]
+	//				configFile.Content = string(buffer)
+	//			}
+	//			return nil
+	//		}
+	//		err = filepath.Walk(path+"/"+subpath.Name(), getFiles)
+	//		if err != nil {
+	//			return nil, err
+	//		}
+	//	case subpath.Name() == "FILTERS":
+	//		if !subpath.IsDir() {
+	//			return nil, fmt.Errorf(path + "/-FILTERS- must be a directory.")
+	//		}
+	//		filters, err := getFiltersFromDirectory(path + "/" + subpath.Name())
+	//		if err != nil {
+	//			glog.Errorf("Error fetching filters at "+path, err)
+	//		} else {
+	//			svc.LogFilters = filters
+	//		}
+	//	case subpath.IsDir():
+	//		subsvc, err := getServiceDefinition(path + "/" + subpath.Name())
+	//		if err != nil {
+	//			return nil, err
+	//		}
+	//		subServices[subpath.Name()] = subsvc
+	//	default:
+	//		glog.Errorf("Unreconised file %s at %s", subpath, path)
+	//	}
+	//}
+	//svc.Services = make([]ServiceDefinition, len(subServices))
+	//i := 0
+	//for _, subsvc := range subServices {
+	//	svc.Services[i] = *subsvc
+	//	i += 1
+	//}
+	//return &svc, err
 }
 
 // this function takes a filter directory and creates a map
