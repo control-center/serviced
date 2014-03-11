d737078f6860315ee21e339ee573e2c8d165a8b1
Rye Terrell <ryeterrell@ryeterrell.net>
2014-03-11 16:36:44 -0500

    fix comment problem

--- serviced/stats.go
+++ serviced/stats.go
@@ -1,11 +1,11 @@
-// // Copyright 2014, The Serviced Authors. All rights reserved.
-// // Use of this source code is governed by a
-// // license that can be found in the LICENSE file.
+// Copyright 2014, The Serviced Authors. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
 
-// // Package agent implements a service that runs on a serviced node. It is
-// // responsible for ensuring that a particular node is running the correct services
-// // and reporting the state and health of those services back to the master
-// // serviced.
+// Package agent implements a service that runs on a serviced node. It is
+// responsible for ensuring that a particular node is running the correct services
+// and reporting the state and health of those services back to the master
+// serviced.
 
 package main
 
