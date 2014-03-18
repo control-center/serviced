230e86606050615ea21c88904ad93fc861731580
daniel-garcia <daniel@danielgarcia.info>
2014-03-18 12:54:27 -0500

    reimplement bounded exponential backoff in terms of exponential backoff

--- coordinator/client/retry/bounded_exponential_backoff.go
+++ coordinator/client/retry/bounded_exponential_backoff.go
@@ -1,25 +1,24 @@
 package retry
 
 import (
-	"math/rand"
 	"time"
 )
 
 type boundedExponentialBackoff struct {
-	baseSleepTime time.Duration // amount of time
-	maxSleepTime  time.Duration
-	maxRetries    int // time between retries
-	done          chan struct{}
+	exponentialBackoff
+	maxSleepTime time.Duration
 }
 
-// ExponentialBackoff returns a policy that will retry up to maxRetries with an exponentially increasing
+// BoundedExponentialBackoff returns a policy that will retry up to maxRetries with an exponentially increasing
 // sleep time up to maxSleepTime.
 func BoundedExponentialBackoff(baseSleepTime time.Duration, maxSleepTime time.Duration, maxRetries int) Policy {
 	return boundedExponentialBackoff{
-		baseSleepTime: baseSleepTime,
-		maxSleepTime:  maxSleepTime,
-		maxRetries:    maxRetries,
-		done:          make(chan struct{}),
+		exponentialBackoff: exponentialBackoff{
+			baseSleepTime: baseSleepTime,
+			maxRetries:    maxRetries,
+			done:          make(chan struct{}),
+		},
+		maxSleepTime: maxSleepTime,
 	}
 }
 
@@ -27,34 +26,10 @@ func (u boundedExponentialBackoff) Name() string {
 	return "BoundedExponentialBackoff"
 }
 
-func (u boundedExponentialBackoff) Close() {
-	// attempt to signal AllowRetry sleep to shutdown.
-	select {
-	case u.done <- struct{}{}:
-	default:
-	}
-}
-
 func (u boundedExponentialBackoff) getSleepTime(retryCount int) time.Duration {
-	sleep := int(rand.Int31n(1<<uint(retryCount) + 1))
-	if sleep < 1 {
-		sleep = 1
-	}
-	sleepTime := u.baseSleepTime * time.Duration(sleep)
+	sleepTime := u.baseSleepTime * u.exponentialBackoff.getSleepTime(retryCount)
 	if sleepTime > u.maxSleepTime {
 		return u.maxSleepTime
 	}
 	return sleepTime
-}
-
-func (u boundedExponentialBackoff) AllowRetry(retryCount int, elapsed time.Duration) bool {
-
-	if retryCount < u.maxRetries {
-		select {
-		case <-time.After(u.getSleepTime(retryCount)):
-			return true
-		case <-u.done:
-		}
-	}
-	return false
 }
