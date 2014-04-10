8d96d5db1472d143e85c74211e53f75014045f7c
Summer Mousa <smousa@zenoss.com>
2014-04-10 17:43:59 -0500

    started work on template commands

--- serviced/client/cmd/cmd_template.go
+++ serviced/client/cmd/cmd_template.go
@@ -14,34 +14,91 @@ func (c *ServicedCli) initTemplate() {
 	})
 	cmd.Commands = []cli.Command{
 		{
-			Name:   "list",
-			Usage:  "Lists all templates.",
-			Action: c.cmdTemplateList,
+			Name:         "list",
+			Usage:        "Lists all templates.",
+			Action:       c.cmdTemplateList,
+			BashComplete: c.printTemplates,
 		}, {
 			Name:   "add",
 			Usage:  "Adds a new template.",
 			Action: c.cmdTemplateAdd,
 		}, {
-			Name:      "remove",
-			ShortName: "rm",
-			Usage:     "Removes an existing template.",
-			Action:    c.cmdTemplateRemove,
+			Name:         "remove",
+			ShortName:    "rm",
+			Usage:        "Removes an existing template.",
+			Action:       c.cmdTemplateRemove,
+			BashComplete: c.printTemplates,
 		}, {
-			Name:   "deploy",
-			Usage:  "Deploys template into a given pool.",
-			Action: c.cmdTemplateDeploy,
+			Name:         "deploy",
+			Usage:        "Deploys template into a given pool.",
+			Action:       c.cmdTemplateDeploy,
+			BashComplete: c.printTemplates,
 		}, {
-			Name:   "compile",
-			Usage:  "Reads a given directory of service definitions to compile to a json struct.",
-			Action: c.cmdTemplateCompile,
+			Name:         "compile",
+			Usage:        "Reads a given directory of service definitions to compile to a json struct.",
+			Action:       c.cmdTemplateCompile,
+			BashComplete: c.printTemplates,
 		},
+	}
+}
+
+// printTemplates is the default completion for each serviced template subcommand
+// usage: serviced template COMMAND --generate-bash-completion
+func (c *ServicedCli) printTemplates(ctx *cli.Context) {
+	if len(ctx.Args()) > 0 {
+		return
+	}
+
+	templates, err := c.driver.ListTemplates()
+	if err != nil || templates == nil || len(templates) == 0 {
+		return
+	}
+
+	for _, t := range templates {
+		fmt.Println(t.ID)
 	}
 }
 
 // cmdTemplateList is the command-line interaction for serviced template list
 // usage: serviced template list
 func (c *ServicedCli) cmdTemplateList(ctx *cli.Context) {
-	fmt.Println("serviced template list")
+	if len(ctx.Args()) > 0 {
+		templateID := ctx.Args()[0]
+		if template, err := c.driver.GetTemplate(templateID); err != nil {
+			fmt.Fprintf(os.Stderr, "error trying to retrieve template: %s\n", err)
+		} else if template == nil {
+			fmt.Fprintln(os.Stderr, "template not found")
+		} else if jsonTemplate, err := json.MarshalIndent(template, " ", "  "); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to marshal template: %s\n", err)
+		} else {
+			fmt.Println(string(jsonTemplate))
+		}
+		return
+	}
+
+	templates, err := c.driver.ListTemplates()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error trying to retrieve templates: %s\n", err)
+		return
+	} else if templates == nil || len(templates) == 0 {
+		fmt.Fprintln(os.Stderr, "no templates found")
+		return
+	}
+
+	if ctx.Bool("verbose") {
+		if jsonTemplate, err := json.MarshalIndent(templates, " ", "  "); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to marshal template list: %s\n", err)
+		} else {
+			fmt.Println(string(jsonTemplate))
+		}
+	} else {
+		tableTemplate := newTable(0, 8, 2)
+		tableTemplate.PrintRow("TEMPLATE ID", "NAME", "DESCRIPTION")
+		for _, t := range templates {
+			tableTemplate.PrintRow(t.ID, t.Name, t.Description)
+		}
+		table.Template.Flush()
+	}
 }
 
 // cmdTemplateAdd is the command-line interaction for serviced template add
