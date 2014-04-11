d03e6ad917c0534eb84397b1d486e6bda4eea71e
Daniel Garcia <daniel@danielgarcia.info>
2014-04-11 17:21:57 -0500

    read content from -CONFIGS- dir, if it exists

--- dao/template.go
+++ dao/template.go
@@ -81,7 +81,7 @@ func getServiceDefinition(path string) (serviceDef *ServiceDefinition, err error
 				if err != nil {
 					return err
 				}
-				path := p[len(path)+len(subpath.Name())+1:]
+				path := p[len(path)+len(subpath.Name()) -1:]
 				if _, ok := svc.ConfigFiles[path]; !ok {
 					svc.ConfigFiles[path] = ConfigFile{
 						Filename: path,
@@ -90,6 +90,7 @@ func getServiceDefinition(path string) (serviceDef *ServiceDefinition, err error
 				} else {
 					configFile := svc.ConfigFiles[path]
 					configFile.Content = string(buffer)
+					svc.ConfigFiles[path] = configFile
 				}
 				return nil
 			}
