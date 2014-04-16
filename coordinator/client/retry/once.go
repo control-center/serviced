7b17aba8df60450b5b7f95cd135c84771b7c914c
daniel-garcia <daniel@danielgarcia.info>
2014-04-16 18:11:44 -0500

    move OneTime to Once

--- coordinator/client/retry/once.go
+++ coordinator/client/retry/once.go
@@ -7,25 +7,25 @@ import (
 	"time"
 )
 
-type oneTime struct {
+type once struct {
 	sleepBetweenRetry time.Duration
 	done              chan struct{}
 }
 
 // OneTime returns a policy that retries only once.
-func OneTime(sleepBetweenRetry time.Duration) Policy {
-	return oneTime{
+func Once(sleepBetweenRetry time.Duration) Policy {
+	return once{
 		sleepBetweenRetry: sleepBetweenRetry,
 		done:              make(chan struct{}),
 	}
 }
 
-func (u oneTime) Name() string {
+func (u once) Name() string {
 	return "OneTime"
 }
 
 // AllowRetry returns true if the retry count is 0.
-func (u oneTime) AllowRetry(retryCount int, elapsed time.Duration) (bool, time.Duration) {
+func (u once) AllowRetry(retryCount int, elapsed time.Duration) (bool, time.Duration) {
 	if retryCount == 0 {
 		return true, u.sleepBetweenRetry
 	}
