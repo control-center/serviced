d9e434da1fd5d84d2762621f393eb0f56a7b33b4
Phil Lu <plu@zenoss.com>
2014-04-16 09:30:12 -0500

    golint modifications

--- serviced/cmd_action.go
+++ serviced/cmd_action.go
@@ -13,7 +13,7 @@ import (
 // runServiceCommand attaches to a service state container and executes an arbitrary bash command
 func attachContainerAndRun(state *dao.ServiceState, command string) ([]byte, error) {
 	if state.DockerId == "" {
-		return []byte{}, errors.New(fmt.Sprintf("DockerId is empty for state:%+v", state))
+		return []byte{}, fmt.Errorf("the DockerId is empty for state:%+v", state)
 	}
 
 	exeMap, err := exePaths([]string{"sudo", "nsinit"})
@@ -21,9 +21,9 @@ func attachContainerAndRun(state *dao.ServiceState, command string) ([]byte, err
 		return []byte{}, err
 	}
 
-	NSINIT_ROOT := "/var/lib/docker/execdriver/native" // has container.json
+	nsInitRoot := "/var/lib/docker/execdriver/native" // has container.json
 
-	attachCmd := fmt.Sprintf("cd %s/%s && %s exec %s", NSINIT_ROOT, state.DockerId,
+	attachCmd := fmt.Sprintf("cd %s/%s && %s exec %s", nsInitRoot, state.DockerId,
 		exeMap["nsinit"], command)
 	fullCmd := []string{exeMap["sudo"], "--", "/bin/bash", "-c", attachCmd}
 	glog.V(2).Infof("ServiceId: %s, Command: %s", state.ServiceId, strings.Join(fullCmd, " "))
@@ -43,7 +43,7 @@ func (cli *ServicedCli) CmdAction(args ...string) error {
 	// Check the args
 	cmd := Subcmd("action", "SERVICEID ACTION", "attach to service instances and perform the predefined action")
 
-	var instance int = -1
+	var instance = -1
 	cmd.IntVar(&instance, "instance", instance, "instance specifier, default -1=ALL")
 
 	if err := cmd.Parse(args); err != nil {
@@ -52,19 +52,19 @@ func (cli *ServicedCli) CmdAction(args ...string) error {
 
 	if len(cmd.Args()) < 2 {
 		cmd.Usage()
-		return errors.New(fmt.Sprintf("missing action to perform\n"))
+		return fmt.Errorf("missing action to perform\n")
 	}
 
-	serviceId := cmd.Arg(0)
+	serviceID := cmd.Arg(0)
 	action := cmd.Arg(1)
 
 	// Get the associated service
 	cp := getClient()
-	service, err := getService(&cp, serviceId)
+	service, err := getService(&cp, serviceID)
 	if err != nil {
 		glog.Fatalf("error while acquiring service - error: %s", err)
 	} else if service == nil {
-		glog.Fatalf("no service found for serviceId: %s", serviceId)
+		glog.Fatalf("no service found for serviceID: %s", serviceID)
 	}
 
 	// Parse the command
@@ -97,7 +97,7 @@ func (cli *ServicedCli) CmdAction(args ...string) error {
 	}
 
 	// TODO: ask control plane to perform action
-	//if err := cp.Action(action, serviceId); err != nil {
+	//if err := cp.Action(action, serviceID); err != nil {
 	//	glog.Errorf("Received an error: %s", err)
 	//	return err
 	//} else {
