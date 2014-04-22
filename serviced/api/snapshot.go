aad7419e5fe5f807342a2acff653d6c7d715896e
Summer Mousa <smousa@zenoss.com>
2014-04-22 13:40:52 -0500

    Added snapshot methods to the api

--- serviced/api/snapshot.go
+++ serviced/api/snapshot.go
@@ -1,6 +1,8 @@
 package api
 
-import ()
+import (
+	"fmt"
+)
 
 const ()
 
@@ -13,25 +15,73 @@ func (a *api) ListSnapshots() ([]string, error) {
 
 // ListSnapshotsByServiceID lists all snapshots for a given service
 func (a *api) ListSnapshotsByServiceID(serviceID string) ([]string, error) {
-	return nil, nil
+	client, err := a.connect()
+	if err != nil {
+		return nil, err
+	}
+
+	var snapshots []string
+	if err := client.Snapshots(serviceID, &snapshots); err != nil {
+		return nil, fmt.Errorf("could not get snapshots: %s", err)
+	}
+
+	return snapshots, nil
 }
 
 // AddSnapshot takes a snapshot for a service
 func (a *api) AddSnapshot(serviceID string) (string, error) {
-	return "", nil
+	client, err := a.connect()
+	if err != nil {
+		return "", err
+	}
+
+	var snapshotID string
+	if err := client.Snapshot(serviceID, &snapshotID); err != nil {
+		return "", fmt.Errorf("could not take snapshot for service: %s", err)
+	}
+
+	return snapshotID, nil
 }
 
 // RemoveSnapshot deletes a snapshot
 func (a *api) RemoveSnapshot(snapshotID string) error {
+	client, err := a.connect()
+	if err != nil {
+		return err
+	}
+
+	if err := client.DeleteSnapshot(snapshotID, &unusedInt); err != nil {
+		return fmt.Errorf("could not remove snapshot: %s", err)
+	}
+
 	return nil
 }
 
 // Commit creates a snapshot and commits it as the service's image
 func (a *api) Commit(dockerID string) (string, error) {
-	return "", nil
+	client, err := a.connect()
+	if err != nil {
+		return "", err
+	}
+
+	var snapshotID string
+	if err := client.Commit(dockerID, &snapshotID); err != nil {
+		return "", fmt.Errorf("could not commit snapshot: %s", err)
+	}
+
+	return snapshotID, nil
 }
 
 // Rollback rolls back the system to the state of the given snapshot
 func (a *api) Rollback(snapshotID string) error {
+	client, err := a.connect()
+	if err != nil {
+		return err
+	}
+
+	if err := client.Rollback(snapshotID, &unusedInt); err != nil {
+		return fmt.Errorf("could not rollback snapshot: %s", err)
+	}
+
 	return nil
 }
