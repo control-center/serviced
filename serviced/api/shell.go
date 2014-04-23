99a43927509e39691b0012b780866713f94df576
Summer Mousa <smousa@zenoss.com>
2014-04-23 07:58:45 -0500

    added shell methods

--- serviced/api/shell.go
+++ serviced/api/shell.go
@@ -1,6 +1,10 @@
 package api
 
-import ()
+import (
+	"fmt"
+
+	"github.com/zenoss/serviced/shell"
+)
 
 const ()
 
@@ -10,16 +14,68 @@ var ()
 type ShellConfig struct {
 	ServiceID string
 	Command   string
+	Args      []string
 	SaveAs    string
 	IsTTY     bool
 }
 
-// ListCommands lists all of the commands for a given service
-func (a *api) ListCommands(id string) ([]string, error) {
-	return nil, nil
-}
-
 // StartShell runs a command for a given service
 func (a *api) StartShell(config ShellConfig) error {
+	command := []string{config.Command}
+	command = append(command, args...)
+
+	cfg := shell.ProcessConfig{
+		ServiceId: config.ServiceID,
+		IsTTY:     config.IsTTY,
+		SaveAs:    config.SaveAs,
+		Command:   strings.Join(command),
+	}
+
+	// TODO: change me to use sockets
+	cmd, err := shell.StartDocker(&cfg, options.port)
+	if err != nil {
+		return fmt.Errorf("failed to connect to service: %s", err)
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
+	return nil
+}
+
+func (a *api) RunShell(config ShellConfig) error {
+	service, err := a.GetService(config.ServiceID)
+	if err != nil {
+		return err
+	}
+
+	var command []string
+	if exec, ok := service.Runs[config.Command]; !ok {
+		return fmt.Errorf("command not found for service")
+	} else {
+		command = []string{exec}
+	}
+	command = append(command, config.Args...)
+
+	cfg := shell.ProcessConfig{
+		ServiceId: config.ServiceID,
+		IsTTY:     config.IsTTY,
+		SaveAs:    config.SaveAs,
+		Command:   fmt.Sprintf("su - zenoss -c \"%s\"", strings.Join(command)),
+	}
+
+	// TODO: change me to use sockets
+	cmd, err := shell.StartDocker(&cfg, options.port)
+	if err != nil {
+		return fmt.Errorf("failed to connect to service: %s", err)
+	}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
 	return nil
 }
