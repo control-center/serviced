6f95e3822a7c97d1358cd05d2b6a1538be1b4508
daniel-garcia <daniel@danielgarcia.info>
2014-04-30 10:07:32 -0500

    don't spin on container kill

--- agent.go
+++ agent.go
@@ -239,7 +239,7 @@ func (a *HostAgent) dockerTerminate(dockerID string) error {
 		return err
 	}
 
-	if err = dc.KillContainer(dockerID); err != nil {
+	if err = dc.KillContainer(dockerID); err != nil && strings.Contains("No such container") {
 		glog.Errorf("unable to kill container %s: %v", dockerID, err)
 		return err
 	}
