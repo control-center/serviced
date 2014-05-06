68727db2b73cf90e78d4ddb387a16fbbe5fcebc7
Daniel Garcia <daniel@danielgarcia.info>
2014-05-06 13:23:12 -0500

    fix bad merge

--- domain/servicedefinition/template_test.go
+++ domain/servicedefinition/template_test.go
@@ -2,7 +2,6 @@ package servicedefinition
 
 import (
 	"github.com/zenoss/serviced/commons"
-	"github.com/zenoss/serviced/domain/service"
 
 	"fmt"
 	"log"
@@ -26,15 +25,15 @@ func init() {
 				ConfigFiles: map[string]ConfigFile{
 					"/etc/my.cnf": ConfigFile{Owner: "root:root", Filename: "/etc/my.cnf", Permissions: "0660", Content: "\n# SAMPLE config file for mysql\n\n[mysqld]\n\ninnodb_buffer_pool_size = 16G\n\n"},
 				},
-				Endpoints: []service.ServiceEndpoint{
-					ServiceEndpoint{
+				Endpoints: []EndpointDefinition{
+					EndpointDefinition{
 						Protocol:    "tcp",
 						PortNumber:  8080,
 						Application: "www",
 						Name:        "www",
 						Purpose:     "export",
 					},
-					ServiceEndpoint{
+					EndpointDefinition{
 						Protocol:    "tcp",
 						PortNumber:  8081,
 						Application: "websvc",
@@ -69,8 +68,8 @@ func init() {
 						Content:     "baz\n",
 					},
 				},
-				Endpoints: []service.ServiceEndpoint{
-					ServiceEndpoint{
+				Endpoints: []EndpointDefinition{
+					EndpointDefinition{
 						Protocol:    "tcp",
 						PortNumber:  8080,
 						Application: "websvc",
@@ -160,7 +159,6 @@ func TestServiceDefinitionFromPath(t *testing.T) {
 
 	sd, err := BuildFromPath("./testsvc")
 
-	t.Logf("testsvc %v", sd)
 	if err != nil {
 		t.Fatalf("Problem parsing template: %s", err)
 	}
