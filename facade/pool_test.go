1e8dd6d41a146d59867b98afae554c11cf2389e8
Jason Huie <jhuie@zenoss.com>
2014-05-06 15:48:39 -0500

    add unit test for commitment

--- facade/pool_test.go
+++ facade/pool_test.go
@@ -231,10 +231,31 @@ func (ft *FacadeTest) Test_PoolCapacity(t *C) {
 	loadedPool, err := ft.Facade.GetResourcePool(ft.CTX, poolid)
 
 	if err != nil {
-	 	t.Fatalf("Unexpected error calculating pool capacity: %v", err)
+		t.Fatalf("Unexpected error calculating pool capacity: %v", err)
 	}
 
 	if loadedPool.CoreCapacity <= 0 || loadedPool.MemoryCapacity <= 0 {
 		t.Fatalf("Unexpected values calculated for %s capacity: CPU - %v : Memory - %v", loadedPool.ID, loadedPool.CoreCapacity, loadedPool.MemoryCapacity)
+	}
+}
+
+func (ft *FacadeTest) Test_PoolCommitment(t *C) {
+	poolid := "pool-id"
+
+	//create pool for test
+	rp := pool.New(poolid)
+	if err := ft.Facade.AddResourcePool(ft.CTX, rp); err != nil {
+		t.Fatalf("Could not add pool for test: %v", err)
+	}
+
+	fmt.Println("%v", h.PoolID)
+
+	// load pool with calculated capacity
+	loadedPool, err := ft.Facade.GetResourcePool(ft.CTX, poolid)
+
+	commitmentErr := ft.Facade.calcPoolCommitment(ft.CTX, loadedPool)
+
+	if commitmentErr != nil {
+		t.Fatalf("Unexpected error calculating pool commitment: %v", err)
 	}
 }
