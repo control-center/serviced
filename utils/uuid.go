e249ba18a45eb28fa8c59f8943c7edab24bf0b7a
Daniel Garcia <daniel@danielgarcia.info>
2014-05-09 14:37:49 -0500

    added uuid

--- utils/uuid.go
+++ utils/uuid.go
@@ -2,26 +2,66 @@
 // Use of this source code is governed by a
 // license that can be found in the LICENSE file.
 
+// base62 uuid taken from https://raw.githubusercontent.com/xhroot/Koderank/48db8afb0759a354bcb16759e2769d3b7621769e/uuid/uuid.go
+// under MIT License
+
 package utils
 
 import (
+	"crypto/rand"
 	"fmt"
-	"os"
+	"io"
+	"math/big"
 )
 
-var urandomFilename = "/dev/urandom"
+const base62alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var randSource io.Reader = randReadT{}
+
+// randReadT is a struct that implements the Reader interface and return random bytes
+type randReadT struct{}
+
+func (r randReadT) Read(p []byte) (n int, err error) {
+	return rand.Read(p)
+}
 
 // NewUUID generate a new UUID
 func NewUUID() (string, error) {
-	f, err := os.Open(urandomFilename)
+	b := make([]byte, 16)
+	_, err := randSource.Read(b)
 	if err != nil {
 		return "", err
 	}
-	b := make([]byte, 16)
-	defer f.Close()
-	f.Read(b)
-	uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-	return uuid, err
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]), nil
 }
 
+// NewUUID62 createa a base-62 UUID.
+func NewUUID62() (string, error) {
+	b := make([]byte, 16)
+	_, err := randSource.Read(b)
+	if err != nil {
+		return "", err
+	}
+	s := fmt.Sprintf("%x", b)
+	return ConvertUp(s, base62alphabet), nil
+}
 
+// ConvertUp converts a hexadecimal UUID string to a base alphabet greater than
+// 16. It is used here to compress a 32 character UUID down to 23 URL friendly
+// characters.
+func ConvertUp(oldNumber string, baseAlphabet string) string {
+	n := big.NewInt(0)
+	n.SetString(oldNumber, 16)
+
+	base := big.NewInt(int64(len(baseAlphabet)))
+
+	newNumber := make([]byte, 23) //converted size of max base-62 uuid
+	i := len(newNumber)
+
+	for n.Int64() != 0 {
+		i--
+		_, r := n.DivMod(n, base, big.NewInt(0))
+		newNumber[i] = baseAlphabet[r.Int64()]
+	}
+	return string(newNumber[i:])
+}
---
+++ utils/uuid_test.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"os"
+	"testing"
+)
+
+type testRandT struct{}
+
+func (t testRandT) Read(p []byte) (n int, err error) {
+	f, err := os.Open("../testfiles/urandom_bytes")
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	return f.Read(p)
+}
+
+// Test newUuid()
+func TestNewUUID(t *testing.T) {
+	randSource = testRandT{}
+
+	uuid, err := NewUUID()
+	if err != nil {
+		t.Errorf("Did not expect error: %s", err)
+		t.Fail()
+	}
+	expectedUuid := "1102c395-e94b-0a08-d1e9-307e31a5213e"
+	if uuid != expectedUuid {
+		t.Errorf("uuid: expected %s, got %s", expectedUuid, uuid)
+		t.Fail()
+	}
+}
+
+// Test newUuid62()
+func TestNewUUID62(t *testing.T) {
+	randSource = testRandT{}
+
+	uuid, err := NewUUID62()
+	if err != nil {
+		t.Errorf("Did not expect error: %s", err)
+		t.Fail()
+	}
+	expectedUuid := "w68e9g0vxRwx0gA9lQBxs"
+	if uuid != expectedUuid {
+		t.Errorf("uuid: expected %s, got %s", expectedUuid, uuid)
+		t.Fail()
+	}
+}
