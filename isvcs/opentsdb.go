9a68dfa21647871ca2c660a74ac9efb3f0107772
daniel-garcia <daniel@danielgarcia.info>
2014-05-20 02:41:34 -0500

    use exec to avoid having bash block signal propagation

--- isvcs/opentsdb.go
+++ isvcs/opentsdb.go
@@ -22,7 +22,7 @@ func init() {
 			Name:    "opentsdb",
 			Repo:    IMAGE_REPO,
 			Tag:     IMAGE_TAG,
-			Command: `cd /opt/zenoss && supervisord -n -c /opt/zenoss/etc/supervisor.conf`,
+			Command: `cd /opt/zenoss && exec supervisord -n -c /opt/zenoss/etc/supervisor.conf`,
 			//only expose 8443 (the consumer port to the host)
 			Ports:   []int{8443, 8888, 9090},
 			Volumes: map[string]string{"hbase": "/opt/zenoss/var/hbase"},
