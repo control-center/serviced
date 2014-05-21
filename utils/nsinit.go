4d18d15ce5afb556894f1a7c1f3b14e843e11486
Ian McCracken <ian.mccracken@gmail.com>
2014-05-21 15:11:58 -0500

    Add nsinit stuff

---
+++ utils/nsinit.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"io/ioutil"
+	"os"
+	"syscall"
+)
+
+var BASH_SCRIPT = `
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+COMMAND="$@"
+trap "rm -f ${DIR}/$$.stderr" EXIT
+for i in {1..10}; do
+	SEEN=0
+	${COMMAND} 2> >(tee ${DIR}/$$.stderr >&2)
+	RESULT=$?
+	[ "${RESULT}" == 0 ] && exit 0
+	grep setns ${DIR}/$$.stderr || exit ${RESULT}
+done
+exit ${RESULT}
+`
+
+func NSInitWithRetry(cmd []string) error {
+	f, err := ioutil.TempFile("", "nsinit")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	defer os.Remove(f.Name())
+	if _, err := f.WriteString(BASH_SCRIPT); err != nil {
+		return err
+	}
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	err = syscall.Exec("/bin/bash", []string{f.Name()}, os.Environ())
+	return nil
+}
