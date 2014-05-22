510ae2801c7185f312770d4dcf8f382aea64769c
Summer Mousa <smousa@zenoss.com>
2014-05-22 14:08:31 -0500

    Cleanup

--- zzk/docker/utils.go
+++ zzk/docker/utils.go
@@ -1,10 +1,7 @@
 package docker
 
 import (
-	"fmt"
-	"os/exec"
 	"path"
-	"strings"
 
 	"github.com/zenoss/serviced/coordinator/client"
 )
@@ -27,32 +24,4 @@ func mkdir(conn client.Connection, dirpath string) error {
 		return err
 	}
 	return conn.CreateDir(dirpath)
-}
-
-func attach(containerID string, command []string) (*exec.Cmd, error) {
-	bash, err := exec.LookPath("bash")
-	if err != nil {
-		return nil, fmt.Errorf("bash not found: %s", err)
-	}
-	nsinit, err := exec.LookPath("nsinit")
-	if err != nil {
-		return nil, fmt.Errorf("nsinit not found: %s", err)
-	}
-	sudo, err := exec.LookPath("sudo")
-	if err != nil {
-		return nil, fmt.Errorf("sudo not found: %s", err)
-	}
-
-	attachCmd := []string{sudo, "--", bash, "-c"}
-	bashcmd := fmt.Sprintf("cd %s/%s && %s exec bash", nsInitRoot, containerID, nsinit)
-	if len(command) > 0 {
-		bashcmd = bashcmd + fmt.Sprintf(" -c \"%s\"", strings.Join(command, " "))
-	}
-	attachCmd = append(attachCmd, bashcmd)
-	fmt.Println(attachCmd)
-	return exec.Command(sudo, attachCmd...), nil
-}
-
-func shell(imageID string, command []string) *exec.Cmd {
-	return nil
 }
