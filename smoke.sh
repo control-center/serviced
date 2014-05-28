a86c98399d41acd919b72f143a008efbb9564b53
Clark Phillips <cphillips@zenoss.com>
2014-05-28 11:09:15 -0500

    strip carriage returns from output

--- smoke.sh
+++ smoke.sh
@@ -103,8 +103,11 @@ test_dir_config() {
 }
 
 test_port_mapped() {
-    [ "$(${SERVICED} service attach s1 wget -qO- http://localhost:9090/etc/bar.txt)" == "baz" ] || return 1
-    return 0
+    varx=`${SERVICED} service attach s1 wget -qO- http://localhost:9090/etc/bar.txt 2>/dev/null | tr -d '\r'| grep -v "^$" | tail -1`
+    if [[ "$varx" == "baz" ]]; then
+        return 0
+    fi
+    return 1
 }
 
 # Force a clean environment
