290b0fe8ef308a3736381b0e74b637c8c35f2a36
jplouis <jplouis@gmail.com>
2014-05-29 18:28:56 -0500

    use container created time for start time

--- agent.go
+++ agent.go
@@ -372,7 +372,7 @@ func (a *HostAgent) waitForProcessToDie(dc *docker.Client, conn coordclient.Conn
 	var sState *servicestate.ServiceState
 	if err = zzk.LoadAndUpdateServiceState(conn, serviceState.ServiceID, serviceState.Id, func(ss *servicestate.ServiceState) {
 		ss.DockerID = ctr.ID
-		ss.Started = time.Now()
+		ss.Started = ctr.Created
 		ss.Terminated = time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC)
 		ss.PrivateIP = ctr.NetworkSettings.IPAddress
 		ss.PortMapping = make(map[string][]domain.HostIPAndPort)
@@ -451,7 +451,7 @@ func (a *HostAgent) waitForProcessToDie(dc *docker.Client, conn coordclient.Conn
 				}
 				if err = zzk.LoadAndUpdateServiceState(conn, serviceState.ServiceID, serviceState.Id, func(ss *servicestate.ServiceState) {
 					ss.DockerID = containerState.ID
-					ss.Started = time.Now()
+					ss.Started = containerState.Created
 					ss.Terminated = time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC)
 					ss.PrivateIP = containerState.NetworkSettings.IPAddress
 					ss.PortMapping = make(map[string][]domain.HostIPAndPort)
