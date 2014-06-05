2f2baba9b9ee4e8ac9a73c5e2c77315601d18dab
Summer Mousa <smousa@zenoss.com>
2014-06-05 18:01:25 -0500

    Need to propogate 'no-registry' to the child containers

--- agent.go
+++ agent.go
@@ -822,7 +822,8 @@ func configureContainer(a *HostAgent, client *ControlClient, conn coordclient.Co
 	cfg.Env = append([]string{},
 		fmt.Sprintf("CONTROLPLANE_SYSTEM_USER=%s", systemUser.Name),
 		fmt.Sprintf("CONTROLPLANE_SYSTEM_PASSWORD=%s", systemUser.Password),
-		fmt.Sprintf("CONTROLPLANE_HOST_IP=%s", ip))
+		fmt.Sprintf("CONTROLPLANE_HOST_IP=%s", ip),
+		fmt.Sprintf("SERVICED_NOREGISTRY=%s", os.Getenv("SERVICED_NOREGISTRY")))
 
 	// add dns values to setup
 	for _, addr := range a.dockerDNS {
