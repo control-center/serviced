62e5a88e14a6bb8728cc266d580c99bc7f526b7c
Phil Lu <plu@zenoss.com>
2014-06-11 12:10:26 -0500

    increase timeout to 180 seconds from 120 to start_serviced

--- smoke.sh
+++ smoke.sh
@@ -43,7 +43,7 @@ start_serviced() {
     echo "Starting serviced..."
     sudo GOPATH=${GOPATH} PATH=${PATH} SERVICED_NOREGISTRY="true" ${PWD}/serviced/serviced -master -agent &
     echo "Waiting 120 seconds for serviced to become the leader..."
-    retry 120 wget --no-check-certificate http://${HOSTNAME}:443 &>/dev/null
+    retry 180 wget --no-check-certificate http://${HOSTNAME}:443 &>/dev/null
     return $?
 }
 
