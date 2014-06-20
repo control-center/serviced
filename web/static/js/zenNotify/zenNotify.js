22ddffb099b1f8693a968f229d395382b826c06b
Jason Huie <jhuie@zenoss.com>
2014-06-20 10:07:24 -0500

    remove redundant setting of array element

--- web/static/js/zenNotify/zenNotify.js
+++ web/static/js/zenNotify/zenNotify.js
@@ -182,7 +182,6 @@
                 this.$storage.forEach(function(el, idx){
                     if(el.id === notification.id){
                         el.read = true;
-                        this.$storage[idx] = el;
                     }
                 }.bind(this));
 
