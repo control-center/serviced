8a19ee151d78931da6093ebf179620ac999259d2
Evan Powell <epowell@zenoss.com>
2014-07-02 16:35:47 -0500

    fmt

--- domain/service/service.go
+++ domain/service/service.go
@@ -36,7 +36,7 @@ type Service struct {
 	ConfigFiles       map[string]servicedefinition.ConfigFile
 	Instances         int
 	InstanceLimits    domain.MinMax
-	ChangeOptions	  []string
+	ChangeOptions     []string
 	ImageID           string
 	PoolID            string
 	DesiredState      int
