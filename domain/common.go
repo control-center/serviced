638930ae61d9f90baa69ff161ca5dabad9655500
Daniel Garcia <daniel@danielgarcia.info>
2014-07-03 17:17:33 -0500

    fixes ZEN-12629, marshal health check interval to seconds (float64) in json

--- domain/common.go
+++ domain/common.go
@@ -5,6 +5,7 @@
 package domain
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
@@ -37,6 +38,31 @@ func (minmax *MinMax) Validate() error {
 type HealthCheck struct {
 	Script   string        // A script to execute to verify the health of a service.
 	Interval time.Duration // The interval at which to execute the script.
+}
+
+type jsonHealthCheck struct {
+	Script   string
+	Interval float64 // the serialzed version will be in seconds
+}
+
+func (hc *HealthCheck) MarshalJSON() ([]byte, error) {
+	// in json, the interval is represented in seconds
+	interval := float64(hc.Interval) / 1000000000.0
+	return json.Marshal(jsonHealthCheck{
+		Script:   hc.Script,
+		Interval: interval,
+	})
+}
+
+func (hc *HealthCheck) UnmarshalJSON(data []byte) error {
+	var tempHc jsonHealthCheck
+	if err := json.Unmarshal(data, &tempHc); err != nil {
+		return err
+	}
+	hc.Script = tempHc.Script
+	// interval in js is in seconds, convert to nanoseconds, then duration
+	hc.Interval = time.Duration(tempHc.Interval * 1000000000.0)
+	return nil
 }
 
 type HealthCheckResult struct {
