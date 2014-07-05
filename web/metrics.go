06807ef61891d3ee661f1abf37ec294988216b99
zenoss <zenoss@mp6.zenoss.loc>
2014-07-05 10:25:07 -0500

    Make MetricID into Datapoints[]

--- web/metrics.go
+++ web/metrics.go
@@ -71,7 +71,7 @@ var (
 				Name:         "Swap empty",
 				Description:  "Alert when swap reaches zero",
 				MetricSource: "memory",
-				MetricID:     "swap.free",
+				DataPoints:   []string{"memory.cached", "memory.free"},
 				Type:         "MinMax",
 				Threshold:    domain.MinMaxThreshold{Min: &zeroInt64, Max: nil},
 				EventTags: map[string]interface{}{
