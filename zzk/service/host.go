79ef53b6ef36388aba807f8aa9ba4b74ad1bb94f
jplouis <jplouis@gmail.com>
2014-07-10 19:44:31 -0500

    time out detaching from host

--- zzk/service/host.go
+++ zzk/service/host.go
@@ -218,6 +218,12 @@ func (l *HostStateListener) listenHostState(shutdown <-chan interface{}, done ch
 			if processDone != nil {
 				glog.V(0).Infof("detaching from %s; %s", hs.ServiceID, hs.ServiceStateID)
 				l.detachInstance(processDone, &state)
+				select {
+				case <-processDone:
+					glog.V(0).Infof("detached from %s; %s", hs.ServiceID, hs.ServiceStateID)
+				case <-Time.After(60 * time.Second):
+					glog.V(0).Infof("timed out detaching from %s; %s", hs.ServiceID, hs.ServiceStateID)
+				}
 
 			} else {
 				glog.V(0).Infof("stopping from %s; %s", hs.ServiceID, hs.ServiceStateID)
