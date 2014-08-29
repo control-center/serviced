7d268045e5db21a59fdc13e5bbbec9d74c6f3acf
Seth Cleveland <scleveland@zenoss.com>
2014-08-29 15:36:37 -0500

    Adding units to graphs

--- domain/monitor_test.go
+++ domain/monitor_test.go
@@ -69,8 +69,165 @@ func TestReBuild(t *testing.T) {
 				Tags: tags,
 			},
 		},
-		ThresholdConfigs: []ThresholdConfig {
-    },
+		ThresholdConfigs: []ThresholdConfig{},
+	}
+
+	if !newProfile.Equals(expectedProfile) {
+		t.Logf("rebuilt profile != expected")
+		t.Logf("newProfile: %+v", newProfile)
+		t.Fatalf("expectedProfile: %+v", expectedProfile)
+	}
+}
+
+func TestReBuildAssignsUnitsToGraphConfigOneDataPoint(t *testing.T) {
+	profile := MonitorProfile{
+		MetricConfigs: []MetricConfig{
+			MetricConfig{
+				ID:   "memory",
+				Name: "Memory Metrics",
+				Metrics: []Metric{
+					Metric{ID: "free", Name: "Free Memory", Unit: "mb"},
+				},
+			},
+		},
+		GraphConfigs: []GraphConfig{
+			GraphConfig{
+				ID: "free.memory",
+				DataPoints: []DataPoint{
+					DataPoint{
+						Metric:       "free",
+						MetricSource: "memory",
+					},
+				},
+			},
+		},
+	}
+
+	tags := map[string][]string{
+		"controlplane_host_id": []string{"1", "2"},
+	}
+
+	newProfile, err := profile.ReBuild("1h-ago", tags)
+	if err != nil {
+		t.Fatalf("Error re building profile=%v", err)
+	}
+
+	headers := make(http.Header)
+	headers["Content-Type"] = []string{"application/json"}
+	expectedProfile := &MonitorProfile{
+		MetricConfigs: []MetricConfig{
+			MetricConfig{
+				ID:   "memory",
+				Name: "Memory Metrics",
+				Query: QueryConfig{
+					RequestURI: "/metrics/api/performance/query",
+					Method:     "POST",
+					Headers:    headers,
+					Data:       "{\"metrics\":[{\"metric\":\"free\",\"tags\":{\"controlplane_host_id\":[\"1\",\"2\"]}}],\"start\":\"1h-ago\"}",
+				},
+				Metrics: []Metric{
+					Metric{ID: "free", Name: "Free Memory", Unit: "mb"},
+				},
+			},
+		},
+		GraphConfigs: []GraphConfig{
+			GraphConfig{
+				ID:    "free.memory",
+				Tags:  tags,
+				Units: "mb",
+				DataPoints: []DataPoint{
+					DataPoint{
+						Metric:       "free",
+						MetricSource: "memory",
+					},
+				},
+			},
+		},
+		ThresholdConfigs: []ThresholdConfig{},
+	}
+
+	if !newProfile.Equals(expectedProfile) {
+		t.Logf("rebuilt profile != expected")
+		t.Logf("newProfile: %+v", newProfile)
+		t.Fatalf("expectedProfile: %+v", expectedProfile)
+	}
+}
+
+func TestReBuildAssignsUnitsToGraphConfig2DataPoints(t *testing.T) {
+	profile := MonitorProfile{
+		MetricConfigs: []MetricConfig{
+			MetricConfig{
+				ID:   "memory",
+				Name: "Memory Metrics",
+				Metrics: []Metric{
+					Metric{ID: "free", Name: "Free Memory", Unit: ""},
+					Metric{ID: "used", Name: "Used Memory", Unit: "mb"},
+				},
+			},
+		},
+		GraphConfigs: []GraphConfig{
+			GraphConfig{
+				ID: "free.memory",
+				DataPoints: []DataPoint{
+					DataPoint{
+						Metric:       "free",
+						MetricSource: "memory",
+					},
+					DataPoint{
+						Metric:       "used",
+						MetricSource: "memory",
+					},
+				},
+			},
+		},
+	}
+
+	tags := map[string][]string{
+		"controlplane_host_id": []string{"1", "2"},
+	}
+
+	newProfile, err := profile.ReBuild("1h-ago", tags)
+	if err != nil {
+		t.Fatalf("Error re building profile=%v", err)
+	}
+
+	headers := make(http.Header)
+	headers["Content-Type"] = []string{"application/json"}
+	expectedProfile := &MonitorProfile{
+		MetricConfigs: []MetricConfig{
+			MetricConfig{
+				ID:   "memory",
+				Name: "Memory Metrics",
+				Query: QueryConfig{
+					RequestURI: "/metrics/api/performance/query",
+					Method:     "POST",
+					Headers:    headers,
+					Data:       "{\"metrics\":[{\"metric\":\"free\",\"tags\":{\"controlplane_host_id\":[\"1\",\"2\"]}},{\"metric\":\"used\",\"tags\":{\"controlplane_host_id\":[\"1\",\"2\"]}}],\"start\":\"1h-ago\"}",
+				},
+				Metrics: []Metric{
+					Metric{ID: "free", Name: "Free Memory", Unit: ""},
+					Metric{ID: "used", Name: "Used Memory", Unit: "mb"},
+				},
+			},
+		},
+		GraphConfigs: []GraphConfig{
+			GraphConfig{
+				ID:    "free.memory",
+				Tags:  tags,
+				Units: "mb",
+				DataPoints: []DataPoint{
+					DataPoint{
+						Metric:       "free",
+						MetricSource: "memory",
+					},
+					DataPoint{
+						Metric:       "used",
+						MetricSource: "memory",
+					},
+				},
+			},
+		},
+		ThresholdConfigs: []ThresholdConfig{},
 	}
 
 	if !newProfile.Equals(expectedProfile) {
