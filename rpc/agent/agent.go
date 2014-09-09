db5a5441a35492393f0f9602746d5d3cfc9f2675
John Crocker <jcrocker@zenoss.com>
2014-09-09 11:29:00 -0500

    Limit docker logs to 2000 lines
    Requires docker 1.1.0

--- rpc/agent/agent.go
+++ rpc/agent/agent.go
@@ -55,16 +55,13 @@ func (a *AgentServer) BuildHost(request BuildHostRequest, hostResponse *host.Hos
 	return nil
 }
 
-// GetDockerLogs returns the last 10k worth of logs from the docker container
+// GetDockerLogs returns the last 2000 lines of logs from the docker container
 func (a *AgentServer) GetDockerLogs(dockerID string, logs *string) error {
-	cmd := exec.Command("docker", "logs", dockerID)
+	cmd := exec.Command("docker", "logs", "--tail=2000", dockerID)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		glog.Errorf("Unable to return logs because: %v", err)
 		return err
-	}
-	if len(output) > 10000 {
-		output = output[len(output)-10000:]
 	}
 	*logs = string(output)
 	return nil
