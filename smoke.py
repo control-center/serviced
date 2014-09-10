893b732177ce720508e12fa69065a4347366b9eb
Daniel Garcia <daniel@danielgarcia.info>
2014-09-09 22:36:29 -0500

    break some docker interactions to functions

--- smoke.py
+++ smoke.py
@@ -19,46 +19,60 @@ import argparse
 import sys
 import time
 
+
+def Try(f, times, sleep=2):
+    ex = None
+    for x in xrange(0, times):
+        try:
+            return f()
+        except Exception as ex:
+            print ex
+        time.sleep(sleep)
+    return None
+
 class ServicedTest(object):
 
     def __init__(self, servicedPath):
         self._servicedPath = servicedPath
 
     def _get_list(self):
-        jsonblob = subprocess.check_output((self._servicedPath, "service", "list", "-v"))
-        return json.loads(jsonblob)
+        return json.loads(subprocess.check_output((self._servicedPath, "service", "list", "-v")))
 
-    def _is_running(self, dockerID):
-        output = subprocess.check_output(("docker", "ps", "--no-trunc"))
-        for line in output.splitlines():
-            if dockerID in line:
-                return True
+    def _docker_ps(self, all=False):
+        args = ["docker", "ps", "-q"]
+	if all:
+            args.append("-a")
+        return subprocess.check_output(args).splitlines()
+
+    def _docker_inspect(self, dockerID):
+        return json.loads(subprocess.check_output(("docker", "inspect", dockerID)))[0]
+
+    def _docker_each_running(self):
+        return { dockerID: self._docker_inspect(dockerID) for dockerID in self._docker_ps() }
+
+    def _serviceIsRunning(self, serviceID, running=None):
+        if running is None:
+            running = self._docker_each_running()
+        for dockerID, container in running.iteritems():
+            for part in container["Config"]["Cmd"]:
+                if serviceID in part:
+                    return True
         return False
 
     def started(self):
-        l = None
-        for x in xrange(0, 5):
-            try:
-                l = self._get_list()
-                break
-            except Exception as ex:
-                print ex
-                pass
-            time.sleep(2)
+        l = Try(self._get_list, 5)
         if l is None:
             print "could not verify if services were started"
             sys.exit(1)
         print "lookging at %d services" % (len(l))
+        running = self._docker_each_running()
         for svc in l:
             if len(svc["Startup"]) == 0:
                 print "skipping %s" % (svc["ID"])
                 continue
             print "looking for status of %s" % (svc["ID"])
-            for x in xrange(0, 5):
-                if self._is_running(svc["ID"]):
-                    print "%s was running" % (svc["ID"])
-                    break
-                time.sleep(2)
+            if self._serviceIsRunning(svc["ID"], running):
+                print "%s was running" % (svc["ID"])
             else:
                 print "svc %s was not found to be running" % (svc["ID"])
                 sys.exit(1)
