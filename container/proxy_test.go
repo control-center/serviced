45f7e1b587919e0e71a5d77473877f67eb140821
Daniel Garcia <daniel@danielgarcia.info>
2014-09-16 16:46:11 -0500

    update test

--- container/proxy_test.go
+++ container/proxy_test.go
@@ -52,7 +52,7 @@ func TestNoMux(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not bind to a port for test")
 	}
-	prxy, err := newProxy("foo", 0, false, local)
+	prxy, err := newProxy("foo", "endpointfoo", 0, false, local)
 	if err != nil {
 		t.Fatalf("Could not create a prxy: %s", err)
 	}
