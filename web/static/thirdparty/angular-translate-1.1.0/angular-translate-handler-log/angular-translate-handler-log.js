aeca2d4a076e0a86a300659bdfd238f8bd63b5eb
Joseph Hanson <jhanson@zenoss.com>
2014-09-18 09:01:52 -0500

    removed old version of angular translate

--- web/static/thirdparty/angular-translate-1.1.0/angular-translate-handler-log/angular-translate-handler-log.js
+++
@@ -1,8 +0,0 @@
-angular.module('pascalprecht.translate').factory('$translateMissingTranslationHandlerLog', [
-  '$log',
-  function ($log) {
-    return function (translationId) {
-      $log.warn('Translation for ' + translationId + ' doesn\'t exist');
-    };
-  }
-]);
