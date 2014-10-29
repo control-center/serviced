ec0de41bacbedc5da4555d96608d2d35c7354602
John <rDr4g0n@gmail.com>
2014-10-29 13:04:53 -0500

    purty new login page

--- web/static/js/controllers/LoginControl.js
+++ web/static/js/controllers/LoginControl.js
@@ -5,17 +5,32 @@ function LoginControl($scope, $location, $notification, $translate, authService)
     }
 
     $scope.brand_label = "CONTROL CENTER";
-    $scope.login_button_text = "Log In";
+
+    enableLoginButton();
 
     // Makes XHR POST with contents of login form
     $scope.login = function() {
+        disableLoginButton();
+
         var creds = { "Username": $scope.username, "Password": $scope.password };
         authService.login(creds, function(){
+            enableLoginButton();
             // Redirect to main page
             $location.path('/apps');
         }, function(){
+            enableLoginButton();
             // display fail message to user
             $notification.create("", $translate.instant("login_fail"), $("#loginNotifications")).error();
         });
     };
+
+    function enableLoginButton(){
+        $scope.login_button_text = $translate.instant("log_in");
+        $scope.loginDisabled = false;
+    }
+
+    function disableLoginButton(){
+        $scope.login_button_text = $translate.instant("logging_in"); 
+        $scope.loginDisabled = true;
+    }
 }
