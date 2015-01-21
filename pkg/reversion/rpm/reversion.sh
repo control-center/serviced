ab5564542e21d31b8c8609b73b89d6f63b264d58
Andrew Lapidas <alapidas@gmail.com>
2015-01-20 20:55:47 -0600

    Change rpm reversion code to use new packaging

--- pkg/reversion/rpm/reversion.sh
+++ pkg/reversion/rpm/reversion.sh
@@ -1,33 +1,37 @@
 #!/bin/sh
 
-if [ "$#" -ne 3 ]
+if [ "$#" -ne 5 ]
 then
 	echo "USAGE: $0 sourceRepoClass sourceVersion targetVersion"
 	echo ""
-	echo "Example - reversion zenoss-unstable 1.0.0_2113 to 1.0.0rc1:"
-	echo "$0 zenoss-unstable 1.0.0_2113 1.0.0rc1"
+	echo "Example - reversion zenoss-unstable 1.0.0-0.0.2113.unstable to 1.0.0-0.1.CR1:"
+	echo "$0 zenoss-unstable 1.0.0 0.0.2113.unstable 1.0.0 0.1.CR1"
 	echo ""
-	echo "Example - reversion testing 1.0.0rc1 to 1.0.0:"
-	echo "$0 zenoss-testing 1.0.0rc1 1.0.0"
+	echo "Example - reversion testing 1.0.0-0.1.CR1 to 1.0.0-1:"
+	echo "$0 zenoss-testing 1.0.0 0.1.RC1 1.0.0 1"
 	exit 1
 fi
 
 SOURCE_CLASS="$1"
 SOURCE_VERSION="$2"
-TARGET_VERSION="$3"
+SOURCE_REVISION="$3"
+TARGET_VERSION="$4"
+TARGET_REVISION="$5"
 
 set -e
 set -x
 
 cd /tmp
-yum install --downloadonly --downloaddir=/tmp --enablerepo=${SOURCE_CLASS} serviced-${SOURCE_VERSION}
+yum install --downloadonly --downloaddir=/tmp --enablerepo=${SOURCE_CLASS} serviced-${SOURCE_VERSION}-${SOURCE_REVISION}
 
-ESCAPED_TARGET=`echo "$TARGET_VERSION" | sed 's/\./\\./g'`
-SED_CMD="s/^Version:.*/Version:${ESCAPED_TARGET}/"
-echo -E "$SED_CMD" >sed.cmd
+ESCAPED_VERSION=`echo "$TARGET_VERSION" | sed 's/\./\\./g'`
+VERSION_SED_CMD="s/^Version:.*/Version:${ESCAPED_VERSION}/"
+ESCAPED_REVISION=`echo "$TARGET_REVISION" | sed 's/\./\\./g'`
+REVISION_SED_CMD="s/^Version:.*/Version:${ESCAPED_REVISION}/"
+echo -E "$VERSION_SED_CMD" >sed.cmd
 
-SOURCE_RPM=serviced-${SOURCE_VERSION}-1.x86_64.rpm
-TARGET_RPM=serviced-${TARGET_VERSION}-1.x86_64.rpm
+SOURCE_RPM=serviced-${SOURCE_VERSION}-${SOURCE_REVISION}.x86_64.rpm
+TARGET_RPM=serviced-${TARGET_VERSION}-${TARGET_REVISION}.x86_64.rpm
 
 echo -e "\nFYI - Here's the metadata for the source package"
 rpm -qip ${SOURCE_RPM}
