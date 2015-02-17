78304bd2d28cdb0de0f666d6357b81d985dcd6f1
Rye Terrell <rterrell@zenoss.com>
2015-02-17 10:12:36 -0600

    Better management of external elastic process during testing.

--- run-tests.sh
+++ run-tests.sh
@@ -4,14 +4,18 @@ ES_VER=0.90.13
 ES_TMP=/tmp/serviced_elastic
 ES_DIR=$ES_TMP/elasticsearch-$ES_VER
 
-if [ -e $ES_TMP/pid ]; then \
-	kill `cat $ES_TMP/pid`; \
-fi
-rm -rf $ES_TMP
+function stop_elastic {
+	if [ -e $ES_TMP/pid ]; then
+		kill `cat $ES_TMP/pid`;
+	fi
+	rm -rf $ES_TMP
+}
+
+stop_elastic
 
 mkdir $ES_TMP
-if [ ! -e /tmp/elasticsearch-$ES_VER.tar.gz ]; then \
-	curl https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VER.tar.gz > /tmp/elasticsearch-$ES_VER.tar.gz; \
+if [ ! -e /tmp/elasticsearch-$ES_VER.tar.gz ]; then
+	curl https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-$ES_VER.tar.gz > /tmp/elasticsearch-$ES_VER.tar.gz;
 fi
 
 tar -xf /tmp/elasticsearch-$ES_VER.tar.gz -C $ES_TMP
@@ -21,9 +25,6 @@ $ES_DIR/bin/elasticsearch -f -Des.http.port=9202 > $ES_TMP/elastic.log & echo $!
 GORACE="history_size=7 halt_on_error=1" go test -race -p 1 ./...
 RESULT=$?
 
-if [ -e $ES_TMP/pid ]; then
-	kill `cat $ES_TMP/pid`;
-fi
-rm -rf $ES_TMP
+stop_elastic
 
 exit $RESULT
