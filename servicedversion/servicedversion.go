ef954d48a1996ad5e813c82f46aee26f94d1475c
Joseph Hanson <jhanson@zenoss.com>
2015-03-03 15:05:09 -0600

    fixes CC-852: cherry-pick #1626 from CC-775 - display serviced version of agents
    https://jira.zenoss.com/browse/CC-857
    
    Conflicts resolved:
    	web/ui/src/Hosts/view-host-details.html
    	web/ui/src/Hosts/view-hosts.html
    	web/ui/static/i18n/en_US.json
    	web/ui/static/i18n/es_US.json

--- servicedversion/servicedversion.go
+++ servicedversion/servicedversion.go
@@ -18,29 +18,76 @@
 
 package servicedversion
 
+import (
+	"github.com/control-center/serviced/utils"
+	"github.com/zenoss/glog"
+
+	"fmt"
+	"os/exec"
+)
+
 var Version string
 var Date string
 var Gitbranch string
 var Gitcommit string
 var Giturl string
 var Buildtag string
+var Release string
 
 type ServicedVersion struct {
-	Version string
-	Date string
+	Version   string
+	Date      string
 	Gitbranch string
 	Gitcommit string
-	Giturl string
-	Buildtag string
+	Giturl    string
+	Buildtag  string
+	Release   string
+}
+
+func init() {
+	var err error
+	Release, err = GetPackageRelease("serviced")
+	if err != nil {
+		glog.Errorf("%s", err)
+	}
 }
 
 func GetVersion() ServicedVersion {
+
 	return ServicedVersion{
-		Version,
-		Date,
-		Gitbranch,
-		Gitcommit,
-		Giturl,
-		Buildtag,
+		Version:   Version,
+		Date:      Date,
+		Gitbranch: Gitbranch,
+		Gitcommit: Gitcommit,
+		Giturl:    Giturl,
+		Buildtag:  Buildtag,
+		Release:   Release,
 	}
+}
+
+// GetPackageRelease returns the release version of the installed package
+func GetPackageRelease(pkg string) (string, error) {
+	command := getCommandToGetPackageRelease(pkg)
+
+	thecmd := exec.Command(command[0], command[1:]...)
+	output, err := thecmd.CombinedOutput()
+	if err != nil {
+		e := fmt.Errorf("unable to retrieve release of package '%s' with command:'%s' output: %s  error: %s\n", pkg, command, output, err)
+		return "", e
+	}
+
+	glog.V(1).Infof("Successfully ran command:'%s' output: %s\n", command, output)
+	return string(output), nil
+}
+
+// getCommandToGetPackageRelease returns the command to get the package release
+func getCommandToGetPackageRelease(pkg string) []string {
+	command := []string{}
+	if utils.Platform == utils.Rhel {
+		command = []string{"bash", "-c", fmt.Sprintf("rpm -q --qf '%%{VERSION}-%%{Release}\n' %s", pkg)}
+	} else {
+		command = []string{"bash", "-c", fmt.Sprintf("dpkg -s %s | awk '/^Version/{print $NF;exit}'", pkg)}
+	}
+
+	return command
 }
