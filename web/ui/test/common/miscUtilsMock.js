01bce9d289abf90ebaafdb853492f5ac3a46d722
John Matos <rDr4g0n@gmail.com>
2015-04-28 13:18:17 -0500

    remove unused table building utils

--- web/ui/test/common/miscUtilsMock.js
+++ web/ui/test/common/miscUtilsMock.js
@@ -5,9 +5,6 @@
 var miscUtilsMock = function($provide) {
     $provide.factory('miscUtils', function() {
         var mock = jasmine.createSpyObj('miscUtils', [
-            'buildTable',
-            'set_order',
-            'get_order_class',
             'unauthorized',
             'indentClass',
             'downloadFile',
