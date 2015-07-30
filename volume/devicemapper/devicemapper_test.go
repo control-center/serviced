205b621647d27c1dd23dac616cb005aba17a0f36
Ian McCracken <ian.mccracken@gmail.com>
2015-07-29 22:27:55 -0500

    Add devicemapper build tags

--- volume/devicemapper/devicemapper_test.go
+++ volume/devicemapper/devicemapper_test.go
@@ -1,3 +1,5 @@
+// +build root daemon
+
 // Copyright 2015 The Serviced Authors.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
