f09d9f71660a73345fa49f60a9c15da75dbdc230
Glenn Jones <gjones@zenoss.com>
2015-08-04 08:19:36 -0500

    Add/adjust tags for files picked up from trunk via rebase

--- volume/btrfs/btrfs_test.go
+++ volume/btrfs/btrfs_test.go
@@ -1,5 +1,3 @@
-// +build root
-
 // Copyright 2014 The Serviced Authors.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build integration
+// +build root,integration
 
 package btrfs_test
 
