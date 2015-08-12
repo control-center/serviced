ec06bea3ff76def4d2bfbc0e93c178df7f18dc1a
Ian McCracken <ian.mccracken@gmail.com>
2015-08-12 11:03:20 -0500

    Clean up logging

--- volume/detect.go
+++ volume/detect.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The Serviced Authors.
+// Copyright 2015 The Serviced Authors.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -23,7 +23,7 @@ import (
 
 func DetectDriverType(root string) (DriverType, error) {
 	// Check to see if the directory even exists. If not, no driver has been initialized.
-	glog.V(0).Infof("Detecting driver type under %s", root)
+	glog.V(2).Infof("Detecting driver type under %s", root)
 	if _, err := os.Stat(root); err != nil {
 		if os.IsNotExist(err) {
 			glog.V(2).Infof("Root does not exist; no driver has been initialized")
@@ -35,8 +35,6 @@ func DetectDriverType(root string) (DriverType, error) {
 	if fi, err := os.Stat(filepath.Join(root, ".devicemapper")); !os.IsNotExist(err) && fi != nil && fi.IsDir() {
 		glog.V(2).Infof("Found .devicemapper directory; returning devicemapper")
 		return DriverTypeDeviceMapper, nil
-	} else {
-		glog.V(0).Infof("Did not find .devicemapper: %+v %s", fi, err)
 	}
 	// Check if there are any volumes
 	fis, err := ioutil.ReadDir(root)
@@ -50,12 +48,12 @@ func DetectDriverType(root string) (DriverType, error) {
 			names = append(names, fi.Name())
 		}
 	}
-	glog.V(2).Infof("Found directories: %s", names)
 	if len(names) == 0 {
 		// No volumes, so essentially no driver
-		glog.V(2).Infof("Found no volumes in the root. Driver not initialized.")
+		glog.V(2).Infof("Found no volumes in the root. Assuming no driver has been initialized.")
 		return "", ErrDriverNotInit
 	}
+	glog.V(2).Infof("Found possible volumes under %s: %s", root, names)
 	// Check to see if it's a btrfs filesystem
 	if IsBtrfsFilesystem(root) {
 		var sudoer bool
@@ -68,7 +66,7 @@ func DetectDriverType(root string) (DriverType, error) {
 		}
 		if _, err := RunBtrFSCmd(sudoer, "subvolume", "show", filepath.Join(root, names[0])); err == nil {
 			// It's btrfs
-			glog.V(0).Infof("Found btrfs filesystem, and a volume is a subvolume")
+			glog.V(2).Infof("Found btrfs filesystem, and a volume is a subvolume; returning btrfs")
 			return DriverTypeBtrFS, nil
 		}
 	}
