df5964d65c122c96145c9df7837f1d9d24ee49f4
Ken Walker <kwalker@zenoss.com>
2015-11-06 13:43:34 -0600

    Fixes gostacktool-11x
    cherry-pick commit 0d1acf87561f813182ba92daa0228a643b0b1d6d
    (committed by "zendev port cherry-pick")

---
+++ tools/gostacktool/gostack/parser.py
@@ -0,0 +1,154 @@
+# Copyright 2015 The Serviced Authors.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import sys
+
+from goroutine import Goroutine
+from stackframe import StackFrame
+from stacktrace import StackTrace
+
+
+class State:
+    findGoroutine = 0
+    getFunction = 1
+    getFile = 2
+    fileEnd = 3
+
+
+class Parser:
+    def __init__(self):
+        self.parsed = False
+        self.filepath = None
+        self.errors = 0
+        self.warnings = 0
+
+    def parse(self, filepath):
+        self.linenum = 0
+        self.stacktrace = StackTrace()
+        self.errors = 0
+        self.warnings = 0
+
+        self.filepath = filepath
+        self.infile = open(filepath, 'r')
+        
+        try:
+            self.state = State.findGoroutine
+
+            for line in self.infile:
+                self.linenum = self.linenum + 1
+                self.processLine(line.strip())
+
+            if self.state != State.findGoroutine:
+                self.finishGoroutine(True)
+        finally:
+            self.infile.close()
+            self.parsed = True
+
+    def processLine(self, line):
+        if self.state == State.findGoroutine:
+            if line.startswith('goroutine'):
+                self.processGoroutineLine(line)
+            # Otherwise, still searching...
+
+        elif self.state == State.getFunction:
+            self.processFunctionLine(line)
+            
+        elif self.state == State.getFile:
+            self.processFileLine(line)
+            
+        elif self.state == State.fileEnd:
+            # Blank line indicates end of this goroutine
+            if not line:
+                self.finishGoroutine(True)
+            # Non-blank line starting with 'goroutine' indicates start of next goroutine
+            elif line.startswith('goroutine'):
+                self.finishGoroutine(True)
+                self.processGoroutineLine(line)
+            # Other non-blank line should be another stack frame
+            else:
+                self.state == State.getFunction
+                self.processFunctionLine(line)
+
+    def processGoroutineLine(self, line):
+        try:
+            self.currentGoroutine = Goroutine()
+
+            warnings = self.currentGoroutine.parseLine(line)
+            self.reportWarnings(warnings)
+
+            self.state = State.getFunction
+
+        except Exception as exc:
+            self.reportError(str(exc))
+            # Skip to next goroutine
+            self.finishGoroutine(False)
+
+    def processFunctionLine(self, line):
+        try:
+            self.currentStackFrame = StackFrame()
+
+            warnings = self.currentStackFrame.parseFunctionLine(line)
+            self.reportWarnings(warnings)
+
+            self.state = State.getFile
+
+        except Exception as exc:
+            self.reportError(str(exc))
+            # Skip to next goroutine
+            self.finishStackFrame(False)
+            self.finishGoroutine(True)
+
+    def processFileLine(self, line):
+        if self.currentStackFrame == None:
+            raise InternalError('No StackFrame instance available!')
+        
+        try:
+            warnings = self.currentStackFrame.parseFileLine(line)
+            self.reportWarnings(warnings)
+
+            self.finishStackFrame(True) # Sets self.state
+
+        except Exception as exc:
+            self.reportError(str(exc))
+            # Skip to next goroutine
+            self.finishStackFrame(False)
+            self.finishGoroutine(True)
+            
+    def finishGoroutine(self, keepIt):
+        if keepIt and self.currentGoroutine != None:
+            self.stacktrace.addGoroutine(self.currentGoroutine)
+        self.currentGoroutine = None
+        self.state = State.findGoroutine
+        
+    def finishStackFrame(self, keepIt):
+        if keepIt and self.currentStackFrame != None:
+            self.currentGoroutine.addFrame(self.currentStackFrame)
+        self.currentStackFrame = None
+        self.state = State.fileEnd
+
+    def addLineNumber(self, msg):
+        return 'Line {0}: {1}'.format(self.linenum, msg)
+
+    def reportWarnings(self, warnings):
+        for warning in warnings:
+            self.reportWarning(warning)
+
+    def reportWarning(self, msg):
+        self.warnings += 1
+        print('WARNING: {0}'.format(self.addLineNumber(msg)), file=sys.stderr)
+
+    def reportError(self, msg):
+        self.errors += 1
+        print('ERROR  : {0}'.format(self.addLineNumber(msg)), file=sys.stderr)
---
+++ tools/gostacktool/gostack/tests/test_goroutine.py
@@ -0,0 +1,211 @@
+# coding: latin-1
+
+# Copyright 2015 The Serviced Authors.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import gostack
+from utils import populateGoroutine
+
+# Possible formats:
+#   goroutine 0 [idle]:
+#   goroutine 1 [chan receive, 30 minutes]:
+#   goroutine 17 [syscall, 31 minutes, locked to thread]:
+#   goroutine 34 [syscall, locked to thread]:
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.goroutine = gostack.Goroutine()
+
+class EqualityBaseTestCase(BaseTestCase):
+    def setUp(self):
+        super(EqualityBaseTestCase, self).setUp()
+        self.goroutine2 = gostack.Goroutine()
+
+#-------------------------------------------
+# Parsing
+
+class Goroutine_GoodWithoutWaitTime(BaseTestCase):
+    def runTest(self):
+        warnings = self.goroutine.parseLine('goroutine 6 [chan receive]:')
+        self.assertEqual(self.goroutine.id, 6)
+        self.assertEqual(self.goroutine.state, 'chan receive')
+        self.assertEqual(self.goroutine.waittime, 0)
+        self.assertFalse(self.goroutine.lockedtothread)
+        self.assertEqual(len(self.goroutine.frames), 0)
+        self.assertEqual(len(warnings), 0)
+
+class Goroutine_GoodWithWaitTime(BaseTestCase):
+    def runTest(self):
+        warnings = self.goroutine.parseLine('goroutine 12 [select, 30 minutes]:')
+        self.assertEqual(self.goroutine.id, 12)
+        self.assertEqual(self.goroutine.state, 'select')
+        self.assertEqual(self.goroutine.waittime, 30)
+        self.assertFalse(self.goroutine.lockedtothread)
+        self.assertEqual(len(self.goroutine.frames), 0)
+        self.assertEqual(len(warnings), 0)
+
+class Goroutine_GoodLockedToThreadWithoutWaitTime(BaseTestCase):
+    def runTest(self):
+        warnings = self.goroutine.parseLine('goroutine 1034 [syscall, locked to thread]:')
+        self.assertEqual(self.goroutine.id, 1034)
+        self.assertEqual(self.goroutine.state, 'syscall')
+        self.assertEqual(self.goroutine.waittime, 0)
+        self.assertTrue(self.goroutine.lockedtothread)
+        self.assertEqual(len(self.goroutine.frames), 0)
+        self.assertEqual(len(warnings), 0)
+
+class Goroutine_GoodLockedToThreadWithWaitTime(BaseTestCase):
+    def runTest(self):
+        warnings = self.goroutine.parseLine('goroutine 5555 [IO wait, 240 minutes, locked to thread]:')
+        self.assertEqual(self.goroutine.id, 5555)
+        self.assertEqual(self.goroutine.state, 'IO wait')
+        self.assertEqual(self.goroutine.waittime, 240)
+        self.assertTrue(self.goroutine.lockedtothread)
+        self.assertEqual(len(self.goroutine.frames), 0)
+        self.assertEqual(len(warnings), 0)
+
+class Goroutine_BadNotGoroutine(BaseTestCase):
+    def runTest(self):
+        with self.assertRaises(gostack.ParseError):
+            self.goroutine.parseLine('blah 12 [select, 30 minutes]:')
+
+class Goroutine_BadNonNumericId(BaseTestCase):
+    def runTest(self):
+        with self.assertRaises(gostack.ParseError):
+            self.goroutine.parseLine('goroutine #49 [chan receive]:')
+
+class Goroutine_BadNoStateWaittime(BaseTestCase):
+    def runTest(self):
+        with self.assertRaises(gostack.ParseError):
+            self.goroutine.parseLine('goroutine 22 something else here:')
+
+class Goroutine_WarnNotMinutes(BaseTestCase):
+    def runTest(self):
+        warnings = self.goroutine.parseLine('goroutine 12 [select, 30 hours]:')
+        self.assertEqual(len(warnings), 1)
+        self.assertTrue(warnings[0].lower().find('unknown field'))
+
+class Goroutine_WarnExtraStateInfoField(BaseTestCase):
+    def runTest(self):
+        warnings = self.goroutine.parseLine('goroutine 8 [chan send, 30 minutes, locked to thread, other]:')
+        self.assertEqual(self.goroutine.id, 8)
+        self.assertEqual(self.goroutine.state, 'chan send')
+        self.assertEqual(self.goroutine.waittime, 30)
+        self.assertTrue(self.goroutine.lockedtothread)
+        self.assertEqual(len(self.goroutine.frames), 0)
+        self.assertEqual(len(warnings), 1)
+        self.assertTrue(warnings[0].lower().find('unknown field'))
+
+#-------------------------------------------
+# Equality
+
+class Goroutine_EqualNoStackFrames(EqualityBaseTestCase):
+    def runTest(self):
+        # All other attributes are different
+        warnings = self.goroutine.parseLine('goroutine 15 [select, 199 minutes]:')
+        self.assertEqual(len(warnings), 0)
+
+        warnings = self.goroutine2.parseLine('goroutine 24829 [IO wait]:')
+        self.assertEqual(len(warnings), 0)
+
+        self.assertTrue(self.goroutine == self.goroutine2)
+        self.assertFalse(self.goroutine != self.goroutine2)
+
+class Goroutine_EqualSameStackFrames(EqualityBaseTestCase):
+    def runTest(self):
+        warnings = populateGoroutine(self.goroutine,
+                                     'goroutine 15 [select, 199 minutes]:',
+                                     'sync.(*WaitGroup).Wait(0xc208110c00)',
+	                             '   /usr/local/go/src/sync/waitgroup.go:132 +0x169',
+                                     'github.com/control-center/go-zookeeper/zk.(*Conn).loop(0xc208129c70)',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:231 +0x76d',
+                                     'github.com/control-center/go-zookeeper/zk.func·001()',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:149 +0x2c',
+                                     'created by github.com/control-center/go-zookeeper/zk.ConnectWithDialer',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:153 +0x4b8')
+        self.assertEqual(len(warnings), 0)
+
+        warnings = populateGoroutine(self.goroutine2,
+                                     'goroutine 24829 [IO wait]:',
+                                     'sync.(*WaitGroup).Wait(0xc208110c00)',
+	                             '   /usr/local/go/src/sync/waitgroup.go:132 +0x169',
+                                     'github.com/control-center/go-zookeeper/zk.(*Conn).loop(0xc208129c70)',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:231 +0x76d',
+                                     'github.com/control-center/go-zookeeper/zk.func·001()',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:149 +0x2c',
+                                     'created by github.com/control-center/go-zookeeper/zk.ConnectWithDialer',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:153 +0x4b8')
+        self.assertEqual(len(warnings), 0)
+
+        self.assertTrue(self.goroutine == self.goroutine2)
+        self.assertFalse(self.goroutine != self.goroutine2)
+
+class Goroutine_NotEqualDifferentStackFrames(EqualityBaseTestCase):
+    def runTest(self):
+        warnings = populateGoroutine(self.goroutine,
+                                     'goroutine 15 [select, 199 minutes]:',
+                                     'sync.(*WaitGroup).Wait(0xc208110c00)',
+	                             '   /usr/local/go/src/sync/waitgroup.go:132 +0x169',
+                                     'github.com/control-center/go-zookeeper/zk.(*Conn).loop(0xc208129c70)',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:231 +0x76d',
+                                     'github.com/control-center/go-zookeeper/zk.func·001()',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:149 +0x2c',
+                                     'created by github.com/control-center/go-zookeeper/zk.ConnectWithDialer',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:153 +0x4b8')
+        self.assertEqual(len(warnings), 0)
+
+        warnings = populateGoroutine(self.goroutine2,
+                                     'goroutine 24829 [IO wait]:',
+                                     'sync.(*WaitGroup).Wait(0xc208110c00)',
+	                             '   /usr/local/go/src/sync/waitgroup.go:132 +0x169',
+                                     'github.com/control-center/go-zookeeper/zk.(*Conn).loop(0xc208129c70)',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:231 +0x76d',
+                                     'github.com/control-center/go-zookeeper/zk.func·001()',
+                                     # Difference is line number below:
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:555 +0x2c',
+                                     'created by github.com/control-center/go-zookeeper/zk.ConnectWithDialer',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:153 +0x4b8')
+        self.assertEqual(len(warnings), 0)
+
+        self.assertFalse(self.goroutine == self.goroutine2)
+        self.assertTrue(self.goroutine != self.goroutine2)
+
+
+class Goroutine_NotEqualDifferentNumStackFrames(EqualityBaseTestCase):
+    def runTest(self):
+        warnings = populateGoroutine(self.goroutine,
+                                     'goroutine 15 [select, 199 minutes]:',
+                                     'sync.(*WaitGroup).Wait(0xc208110c00)',
+	                             '   /usr/local/go/src/sync/waitgroup.go:132 +0x169',
+                                     'github.com/control-center/go-zookeeper/zk.(*Conn).loop(0xc208129c70)',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:231 +0x76d',
+                                     'github.com/control-center/go-zookeeper/zk.func·001()',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:149 +0x2c',
+                                     'created by github.com/control-center/go-zookeeper/zk.ConnectWithDialer',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:153 +0x4b8')
+        self.assertEqual(len(warnings), 0)
+
+        warnings = populateGoroutine(self.goroutine2,
+                                     'goroutine 24829 [IO wait]:',
+                                     'sync.(*WaitGroup).Wait(0xc208110c00)',
+	                             '   /usr/local/go/src/sync/waitgroup.go:132 +0x169',
+                                     'github.com/control-center/go-zookeeper/zk.(*Conn).loop(0xc208129c70)',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:231 +0x76d',
+                                     'github.com/control-center/go-zookeeper/zk.func·001()',
+	                             '   /home/kwalker/src/europa/src/golang/src/github.com/control-center/serviced/Godeps/_workspace/src/github.com/control-center/go-zookeeper/zk/conn.go:153 +0x4b8')
+        self.assertEqual(len(warnings), 0)
+
+        self.assertFalse(self.goroutine == self.goroutine2)
+        self.assertTrue(self.goroutine != self.goroutine2)
