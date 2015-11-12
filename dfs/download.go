3c88542e9564a4713877fe5e5c0869f44245cb1d
Summer Mousa <smousa@zenoss.com>
2015-11-12 11:35:50 -0600

    RegistryIndex does not pass forward NoSuchEntity errors and download does an extra check to verify the service image

--- dfs/download.go
+++ dfs/download.go
@@ -17,9 +17,10 @@ import (
 	"errors"
 
 	"github.com/control-center/serviced/commons"
-	"github.com/control-center/serviced/datastore"
 	"github.com/control-center/serviced/dfs/docker"
+	index "github.com/control-center/serviced/dfs/registry"
 	"github.com/control-center/serviced/domain/registry"
+	dockerclient "github.com/fsouza/go-dockerclient"
 	"github.com/zenoss/glog"
 )
 
@@ -30,6 +31,74 @@ var (
 // Download will download the image from upstream and save the image to the
 // registry.
 func (dfs *DistributedFilesystem) Download(image, tenantID string, upgrade bool) (string, error) {
+	if !upgrade {
+		// Is this an image that has already been deployed?
+		if rImage, err := dfs.findImage(image, tenantID); err != nil {
+			return "", err
+		} else if rImage != "" {
+			return rImage, nil
+		}
+	}
+	// Figure out what the image will be in the docker registry
+	rImage, err := dfs.parseRegistryImage(image, tenantID)
+	if err != nil {
+		return "", err
+	}
+	// Pull the image (if it doesn't exist locally)
+	img, err := dfs.pullImage(image)
+	if err != nil {
+		return "", err
+	}
+	rimg, err := dfs.index.FindImage(rImage)
+	if err == index.ErrImageNotFound {
+		// Image does not exist in the registry, so push
+		if err := dfs.index.PushImage(rImage, img.ID); err != nil {
+			glog.Errorf("Could not push image %s into registry: %s", rImage, err)
+			return "", err
+		}
+		return rImage, nil
+	} else if err != nil {
+		glog.Errorf("Could not look up image %s from the registry: %s", rImage, err)
+		return "", err
+	}
+	// Compare the uuids of the topmost layer
+	if rimg.UUID != img.ID {
+		if upgrade {
+			// We are upgrading the image, so overwrite the existing tag with
+			// the new UUID.
+			if err := dfs.index.PushImage(rImage, img.ID); err != nil {
+				glog.Errorf("Could not upgrade image %s into registry: %s", rImage, err)
+				return "", err
+			}
+		} else {
+			return "", ErrImageCollision
+		}
+	}
+	return rImage, nil
+}
+
+// findImage will verify whether the image has already been deployed with the
+// application.
+func (dfs *DistributedFilesystem) findImage(image, tenantID string) (string, error) {
+	// Is the image in the index?
+	rImage, err := dfs.index.FindImage(image)
+	if err != nil {
+		if err == index.ErrImageNotFound {
+			return "", nil
+		}
+		glog.Errorf("Could not look up image %s from the registry: %s", image, err)
+		return "", err
+	}
+	// Is this image latest under the same tenant?
+	if rImage.Library == tenantID && rImage.Tag == docker.Latest {
+		return rImage.String(), nil
+	}
+	return "", nil
+}
+
+// parseRegistryImage formats the image as it will be written into the docker
+// registry.
+func (dfs *DistributedFilesystem) parseRegistryImage(image, tenantID string) (string, error) {
 	// Is this a proper docker image?
 	imageID, err := commons.ParseImageID(image)
 	if err != nil {
@@ -42,39 +111,26 @@ func (dfs *DistributedFilesystem) Download(image, tenantID string, upgrade bool)
 		Repo:    imageID.Repo,
 		Tag:     docker.Latest,
 	}).String()
+	return rImage, nil
+}
+
+// pullImage pulls the image from the upstream if it doesn't already exist
+// locally
+func (dfs *DistributedFilesystem) pullImage(image string) (*dockerclient.Image, error) {
 	// Find (or download) the image
 	img, err := dfs.docker.FindImage(image)
 	if docker.IsImageNotFound(err) {
 		glog.Infof("Image %s not found locally, pulling", image)
 		if err := dfs.docker.PullImage(image); err != nil {
 			glog.Errorf("Could not pull image %s: %s", image, err)
-			return "", err
+			return nil, err
 		} else if img, err = dfs.docker.FindImage(image); err != nil {
 			glog.Errorf("Could not find image %s: %s", image, err)
-			return "", err
+			return nil, err
 		}
 	} else if err != nil {
 		glog.Errorf("Could not find image %s: %s", image, err)
-		return "", err
+		return nil, err
 	}
-	// Compare the uuids of the topmost layer
-	if rimg, err := dfs.index.FindImage(rImage); datastore.IsErrNoSuchEntity(err) {
-		if err := dfs.index.PushImage(rImage, img.ID); err != nil {
-			glog.Errorf("Could not push image %s into registry: %s", rImage, err)
-			return "", err
-		}
-	} else if err != nil {
-		glog.Errorf("Could not look up image %s: %s", rImage, err)
-		return "", err
-	} else if rimg.UUID != img.ID {
-		if upgrade {
-			if err := dfs.index.PushImage(rImage, img.ID); err != nil {
-				glog.Errorf("Could not push image %s into registry: %s", rImage, err)
-				return "", err
-			}
-		} else {
-			return "", ErrImageCollision
-		}
-	}
-	return rImage, nil
+	return img, nil
 }
