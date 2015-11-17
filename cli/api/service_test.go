0d875a281dcb4ee99bd441685c1bda01b960ffdc
Glenn Jones <gjones@zenoss.com>
2015-11-17 14:26:43 -0600

    Use gocheck for all of cli/api/*_test.go

--- cli/api/service_test.go
+++ cli/api/service_test.go
@@ -17,74 +17,60 @@ package api
 
 import (
 	"errors"
-	"testing"
 
-	daomocks "github.com/control-center/serviced/dao/mocks"
-	"github.com/control-center/serviced/domain/service"
-	"github.com/control-center/serviced/rpc/master/mocks"
+	"strings"
+
+//	"github.com/control-center/serviced/dao"
+//	"github.com/control-center/serviced/domain/service"
 
 	"github.com/control-center/serviced/domain/applicationendpoint"
 	"github.com/stretchr/testify/mock"
 	. "gopkg.in/check.v1"
 )
 
-var tGlobal *testing.T
-
-// This plumbs gocheck into testing
-func Test(t *testing.T) {
-	TestingT(t)
-
-	// HACK ALERT: Some functions in testify.Mock require testing.T, but
-	//	gocheck doesn't offer access to it, so we have to save a copy in
-	//	a global variable :=(
-	tGlobal = t
-}
-
-var _ = Suite(&serviceAPITest{})
-
-type serviceAPITest struct {
-	api API
-
-	//  A mock implementation of the ControlPlane interface
-	mockControlPlane *daomocks.ControlPlane
-
-	mockMasterClient *mocks.ClientInterface
-}
-
-func (st *serviceAPITest) SetUpTest(c *C) {
-	st.mockControlPlane = &daomocks.ControlPlane{}
-	st.mockMasterClient = &mocks.ClientInterface{}
-
-	apiObj := NewAPI(st.mockMasterClient, nil, nil, st.mockControlPlane)
-	st.api = apiObj
-}
-
-func (st *serviceAPITest) TearDownTest(c *C) {
-	// don't allow per-test-case values to be reused across test cases
-	st.api = nil
-	st.mockControlPlane = nil
-}
-
-func (st *serviceAPITest) TestGetService_works(c *C) {
+func (s *CliAPITestSuite) TestGetService_works(c *C) {
 	serviceID := "test-service"
 	expected, _ := service.NewService()
 
-	st.mockControlPlane.On("GetService", serviceID, mock.AnythingOfType("*service.Service")).Return(nil).Run(func(a mock.Arguments) {
+	s.mockControlPlane.On("GetService", serviceID, mock.AnythingOfType("*service.Service")).Return(nil).Run(func(a mock.Arguments) {
 		svc := a.Get(1).(*service.Service)
 		*svc = *expected
 	})
-	actual, err := st.api.GetService(serviceID)
+	actual, err := s.api.GetService(serviceID)
 	c.Assert(err, IsNil)
 	c.Assert(actual.ID, Equals, expected.ID)
 }
 
-func (st *serviceAPITest) TestGetService_fails(c *C) {
+func (s *CliAPITestSuite) TestGetService_fails(c *C) {
 	errorStub := errors.New("errorStub: GetService() failed")
 	serviceID := "test-service"
-	st.mockControlPlane.On("GetService", serviceID, mock.AnythingOfType("*service.Service")).Return(errorStub)
-	actual, err := st.api.GetService(serviceID)
+	s.mockControlPlane.On("GetService", serviceID, mock.AnythingOfType("*service.Service")).Return(errorStub)
+	actual, err := s.api.GetService(serviceID)
 	c.Assert(actual, IsNil)
 	c.Assert(err, Equals, errorStub)
+}
+
+func (s *CliAPITestSuite) TestMigrateService_works(c *C) {
+	serviceID := "test-service"
+	scriptBody := "# no-op script"
+	inputScript := strings.NewReader(scriptBody)
+	expected, _ := service.NewService()
+	sdkVersion := "a.b.c"
+	s.mockControlPlane.On("GetService", serviceID, mock.AnythingOfType("*service.Service")).Return(nil).Run(func(a mock.Arguments) {
+		svc := a.Get(1).(*service.Service)
+		*svc = *expected
+	})
+	s.mockControlPlane.On("RunMigrationScript", mock.AnythingOfType("dao.RunMigrationScriptRequest"), mock.AnythingOfType("*int")).Return(nil).Run(func(a mock.Arguments) {
+		req := a.Get(0).(dao.RunMigrationScriptRequest)
+		c.Assert(req.ServiceID, Equals, serviceID)
+		c.Assert(req.ScriptBody, Equals, scriptBody)
+		c.Assert(req.DryRun, Equals, true)
+		c.Assert(req.SDKVersion, Equals, sdkVersion)
+	})
+	actual, err := s.api.RunMigrationScript(serviceID, inputScript, true, sdkVersion)
+	c.Assert(err, IsNil)
+	c.Assert(actual.ID, Equals, expected.ID)
+	s.mockControlPlane.AssertExpectations(c)
 }
 
 type mockInputReader struct {
@@ -96,28 +82,28 @@ func (m mockInputReader) Read(p []byte) (n int, err error) {
 	return args.Int(0), args.Error(1)
 }
 
-func (st *serviceAPITest) TestGetEndpoints_fails(c *C) {
+func (s *CliAPITestSuite) TestGetEndpoints_fails(c *C) {
 	errorStub := errors.New("errorStub: GetServiceEndpoints() failed")
 	serviceID := "test-service"
 
-	st.mockMasterClient.
+	s.mockMasterClient.
 		On("GetServiceEndpoints", []string{serviceID}, true, true, true).
 		Return(nil, errorStub)
 
-	actual, err := st.api.GetEndpoints(serviceID, true, true, true)
+	actual, err := s.api.GetEndpoints(serviceID, true, true, true)
 
 	c.Assert(actual, IsNil)
 	c.Assert(err, Equals, errorStub)
 }
 
-func (st *serviceAPITest) TestGetEndpoints_works(c *C) {
+func (s *CliAPITestSuite) TestGetEndpoints_works(c *C) {
 	serviceID := "test-service"
 
-	st.mockMasterClient.
+	s.mockMasterClient.
 		On("GetServiceEndpoints", []string{serviceID}, true, true, true).
 		Return([]applicationendpoint.EndpointReport{}, nil)
 
-	actual, err := st.api.GetEndpoints(serviceID, true, true, true)
+	actual, err := s.api.GetEndpoints(serviceID, true, true, true)
 
 	c.Assert(err, IsNil)
 	c.Assert(actual, NotNil)
