e938b1f6afafdd0913d7d16a12e8787e7349e8f1
Ian McCracken <ian.mccracken@gmail.com>
2015-12-08 14:45:41 -0600

    Refactoring

--- health/health.go
+++ health/health.go
@@ -32,25 +32,15 @@ import (
 
 var (
 	// Map of ServiceID -> InstanceID -> HealthCheckName -> healthStatus
-	healthStatuses  = make(map[string]map[string]map[string]*domain.HealthCheckStatus)
-	cpDao           dao.ControlPlane
-	runningServices []dao.RunningService
-	exitChannel     = make(chan bool)
-	lock            = &sync.RWMutex{}
-	healthMap       = NewHealthStatuses()
+	healthStatuses = make(map[string]map[string]map[string]*domain.HealthCheckStatus)
+	cpDao          dao.ControlPlane
+	exitChannel    = make(chan bool)
+	lock           = &sync.RWMutex{}
+	healthMap      *HealthStatusMap
 )
 
-func init() {
-	// Initialize the fake isvc application.
-	healthStatuses["isvc-internalservices"] = map[string]map[string]*domain.HealthCheckStatus{
-		"0": {
-			"alive": &domain.HealthCheckStatus{
-				Status:    "passed",
-				Timestamp: time.Now().UTC().Unix(),
-				Interval:  3.156e9, // One century in seconds.
-			},
-		},
-	}
+func now() int64 {
+	return time.Now().UTC().Unix()
 }
 
 type messagePacket struct {
@@ -58,7 +48,28 @@ type messagePacket struct {
 	Statuses  map[string]map[string]map[string]*domain.HealthCheckStatus
 }
 
-type healthStatusMap struct {
+type RunningServices []dao.RunningService
+
+func (r RunningServices) getService(serviceID, instanceID string) *dao.RunningService {
+	for _, svc := range r {
+		if svc.ServiceID == serviceID && strconv.Itoa(svc.InstanceID) == instanceID {
+			return &svc
+		}
+	}
+	return nil
+}
+
+func (r RunningServices) isService(serviceID string) bool {
+	for _, svc := range r {
+		if svc.ServiceID == serviceID {
+			return true
+		}
+	}
+	return false
+}
+
+// HealthStatusMap
+type HealthStatusMap struct {
 	sync.RWMutex
 	cpDao        dao.ControlPlane
 	facade       facade.FacadeInterface
@@ -66,60 +77,106 @@ type healthStatusMap struct {
 	statuses     map[string]map[string]map[string]*domain.HealthCheckStatus
 }
 
-func NewHealthStatuses(cpDao dao.ControlPlane, f facade.FacadeInterface) *healthStatusMap {
-	return &healthStatusMap{
+func NewHealthStatuses(cpDao dao.ControlPlane, f facade.FacadeInterface) *HealthStatusMap {
+	status := &HealthStatusMap{
 		cpDao:        cpDao,
 		facade:       f,
 		statuses:     make(map[string]map[string]map[string]*domain.HealthCheckStatus),
 		serviceLocks: utils.NewMutexMap(),
 	}
-}
-
-func (m *healthStatusMap) SetHealthStatus(serviceID, instanceID, healthCheckName, status string, interval float64) {
-	if healthCheckName == "__instance_shutdown" {
-		m.deleteServiceInstance(serviceID, instanceID)
-		return
+	status.statuses["isvc-internalservices"] = map[string]map[string]*domain.HealthCheckStatus{
+		"0": {
+			"alive": &domain.HealthCheckStatus{
+				Status:    "passed",
+				Timestamp: now(),
+				Interval:  3.156e9, // One century in seconds.
+			},
+		},
 	}
-	instance := m.getServiceInstance(serviceID, instanceID)
+	return status
 }
 
-func (m *healthStatusMap) deleteServiceInstance(serviceID, instanceID string) {
+// deleteServiceInstance deletes a single instance from the map
+func (m *HealthStatusMap) deleteServiceInstance(serviceID, instanceID string) {
 	m.serviceLocks.LockKey(serviceID)
 	defer m.serviceLocks.UnlockKey(serviceID)
-	serviceStatus, ok := healthStatuses[serviceID]
+	serviceStatus, ok := m.statuses[serviceID]
 	if !ok {
 		serviceStatus = make(map[string]map[string]*domain.HealthCheckStatus)
-		healthStatuses[serviceID] = serviceStatus
+		m.statuses[serviceID] = serviceStatus
 	}
 	delete(serviceStatus, instanceID)
 }
 
-func (m *healthStatusMap) getServiceInstance(serviceID, instanceID string) map[string]*domain.HealthCheckStatus {
+// deleteServiceInstance deletes an entire service from the map
+func (m *HealthStatusMap) deleteService(serviceID string) {
 	m.serviceLocks.LockKey(serviceID)
 	defer m.serviceLocks.UnlockKey(serviceID)
-	serviceStatus, ok := healthStatuses[serviceID]
+	delete(m.statuses, serviceID)
+}
+
+// getOrCreateServiceInstance looks up a service instance in the map. If it
+// does not exist, it creates the necessary data structures populated with
+// default values.
+func (m *HealthStatusMap) getOrCreateServiceInstance(serviceID, instanceID string) (instanceStatus map[string]*domain.HealthCheckStatus) {
+	var ok bool
+	m.serviceLocks.LockKey(serviceID)
+	defer m.serviceLocks.UnlockKey(serviceID)
+	serviceStatus, ok := m.statuses[serviceID]
 	if !ok {
 		serviceStatus = make(map[string]map[string]*domain.HealthCheckStatus)
-		healthStatuses[serviceID] = serviceStatus
+		m.statuses[serviceID] = serviceStatus
 	}
-	instanceStatus, ok := serviceStatus[instanceID]
+	instanceStatus, ok = serviceStatus[instanceID]
 	if !ok {
 		instanceStatus = make(map[string]*domain.HealthCheckStatus)
 		serviceStatus[instanceID] = instanceStatus
+		healthChecks, err := m.facade.GetHealthChecksForService(datastore.Get(), serviceID)
+		if err != nil {
+			glog.Errorf("Unable to acquire health checks: %+v", err)
+			return
+		}
+		for iname, icheck := range healthChecks {
+			instanceStatus[iname] = &domain.HealthCheckStatus{
+				iname,
+				"unknown",
+				0,
+				icheck.Interval.Seconds(),
+				now(),
+				"",
+			}
+		}
 	}
-	return instanceStatus
+	return
 }
 
-func (m *healthStatusMap) initializeHealthChecks(serviceID, instanceID string, f facade.FacadeInterface) {
-
+// SetHealthStatus sets the status for a health check in the map.
+func (m *HealthStatusMap) SetHealthStatus(serviceID, instanceID, healthCheckName, status string) {
+	if healthCheckName == "__instance_shutdown" {
+		m.deleteServiceInstance(serviceID, instanceID)
+		return
+	}
+	instanceStatus := m.getOrCreateServiceInstance(serviceID, instanceID)
+	thisStatus, ok := instanceStatus[healthCheckName]
+	if !ok {
+		glog.Warningf("ignoring %s health status %s, not found in service %s", status, healthCheckName, serviceID)
+		return
+	}
+	newTime := now()
+	// This is technically a race, but so unlikely a race that it's not worth
+	// locking the entire service
+	if newTime >= thisStatus.Timestamp {
+		thisStatus.Status = status
+		thisStatus.Timestamp = newTime
+	}
 }
 
 // Returns Map of InstanceID -> HealthCheckName -> healthStatus for a given serviceID.
-func (m *healthStatusMap) GetHealthStatusesForService(serviceID string) map[string]map[string]domain.HealthCheckStatus {
+func (m *HealthStatusMap) GetHealthStatusesForService(serviceID string) map[string]map[string]domain.HealthCheckStatus {
 	m.serviceLocks.RLockKey(serviceID)
 	defer m.serviceLocks.RUnlockKey(serviceID)
-	// Make a copy of healthStatuses[serviceID] and store the HealthCheckStatus values instead of pointers
-	result := make(map[string]map[string]domain.HealthCheckStatus, len(healthStatuses[serviceID]))
+	// Make a copy of m.statuses[serviceID] and store the HealthCheckStatus values instead of pointers
+	result := make(map[string]map[string]domain.HealthCheckStatus, len(m.statuses[serviceID]))
 	for instanceID, healthChecks := range m.statuses[serviceID] {
 		result[instanceID] = make(map[string]domain.HealthCheckStatus, len(healthChecks))
 		for hcName, hcStatus := range healthChecks {
@@ -130,62 +187,36 @@ func (m *healthStatusMap) GetHealthStatusesForService(serviceID string) map[stri
 
 }
 
-func GetHealthStatusesForService(serviceID string) map[string]map[string]domain.HealthCheckStatus {
-
-	//make a copy of healthStatuses[serviceID] and store the HealthCheckStatus values instead of pointers
-	result := make(map[string]map[string]domain.HealthCheckStatus, len(healthStatuses[serviceID]))
-	for instanceID, healthChecks := range healthStatuses[serviceID] {
-		result[instanceID] = make(map[string]domain.HealthCheckStatus, len(healthChecks))
-		for hcName, hcStatus := range healthChecks {
-			result[instanceID][hcName] = *hcStatus
-		}
+func (m *HealthStatusMap) getRunningServices() (*RunningServices, error) {
+	var empty interface{}
+	rs := []dao.RunningService{}
+	if err := m.cpDao.GetRunningServices(&empty, &rs); err != nil {
+		return nil, err
 	}
-	return result
-}
-
-func getService(serviceID, instanceID string) *dao.RunningService {
-	for _, svc := range runningServices {
-		if svc.ServiceID == serviceID && strconv.Itoa(svc.InstanceID) == instanceID {
-			return &svc
-		}
-	}
-	return nil
-}
-
-func isService(serviceID string) bool {
-	for _, svc := range runningServices {
-		if svc.ServiceID == serviceID {
-			return true
-		}
-	}
-	return false
+	r := RunningServices(rs)
+	return &r, nil
 }
 
 // CleanupOnce removes services that are no longer running and updates service
 // start time.
-func CleanupOnce() {
-	var empty interface{}
-	if cpDao == nil {
-		return
-	}
-	err := cpDao.GetRunningServices(&empty, &runningServices)
+func (m *HealthStatusMap) CleanupOnce() {
+	runningServices, err := m.getRunningServices()
 	if err != nil {
 		glog.Warningf("Error acquiring running services: %v", err)
 		return
 	}
-	lock.Lock()
-	for serviceID, instances := range healthStatuses {
+	for serviceID, instances := range m.statuses {
 		if strings.HasPrefix(serviceID, "isvc-") {
 			continue
 		}
-		if !isService(serviceID) {
-			delete(healthStatuses, serviceID)
+		if !runningServices.isService(serviceID) {
+			m.deleteService(serviceID)
 			continue
 		}
 		for instanceID, healthChecks := range instances {
-			svc := getService(serviceID, instanceID)
+			svc := runningServices.getService(serviceID, instanceID)
 			if svc == nil {
-				delete(instances, instanceID)
+				m.deleteServiceInstance(serviceID, instanceID)
 				continue
 			}
 			for _, check := range healthChecks {
@@ -195,28 +226,28 @@ func CleanupOnce() {
 			}
 		}
 	}
-	lock.Unlock()
 }
 
 // Cleanup runs CleanupOnce on a five-second cycle.
-func Cleanup(shutdown <-chan interface{}) {
+func (m *HealthStatusMap) Cleanup(shutdown <-chan interface{}) {
 	for {
 		select {
 		case <-shutdown:
 			return
 		case <-time.After(time.Second * 5):
-			CleanupOnce()
+			m.CleanupOnce()
 		}
 	}
 }
 
 // Stores the dao.ControlPlane object created in daemon.go for use in this module.
-func SetDao(d dao.ControlPlane) {
-	cpDao = d
+func Initialize(d dao.ControlPlane, f facade.FacadeInterface, shutdown <-chan interface{}) {
+	healthMap = NewHealthStatuses(d, f)
+	go healthMap.Cleanup(shutdown)
 }
 
 // RestGetHealthStatus writes a JSON response with the health status of all services that have health checks.
-func RestGetHealthStatus(w *rest.ResponseWriter, r *rest.Request, client *node.ControlClient) {
+func (m *HealthStatusMap) RestGetHealthStatus(w *rest.ResponseWriter, r *rest.Request, client *node.ControlClient) {
 	lock.RLock()
 	defer lock.RUnlock()
 	isvcNames := isvcs.Mgr.GetServiceNames()
@@ -233,47 +264,6 @@ func RestGetHealthStatus(w *rest.ResponseWriter, r *rest.Request, client *node.C
 			healthStatuses[iname]["0"][status2.Name] = &status2
 		}
 	}
-	packet := messagePacket{time.Now().UTC().Unix(), healthStatuses}
+	packet := messagePacket{now(), healthStatuses}
 	w.WriteJson(&packet)
-}
-
-// RegisterHealthCheck updates the healthStatus and healthTime structures with a health check result.
-func RegisterHealthCheck(serviceID string, instanceID string, name string, passed string, f facade.FacadeInterface) {
-	lock.Lock()
-	defer lock.Unlock()
-	serviceStatus, ok := healthStatuses[serviceID]
-	if !ok {
-		serviceStatus = make(map[string]map[string]*domain.HealthCheckStatus)
-		healthStatuses[serviceID] = serviceStatus
-	}
-	instanceStatus, ok := serviceStatus[instanceID]
-	if !ok {
-		instanceStatus = make(map[string]*domain.HealthCheckStatus)
-		serviceStatus[instanceID] = instanceStatus
-	}
-	if name == "__instance_shutdown" {
-		delete(serviceStatus, instanceID)
-		return
-	}
-	thisStatus, ok := instanceStatus[name]
-	if !ok {
-		healthChecks, err := f.GetHealthChecksForService(datastore.Get(), serviceID)
-		if err != nil {
-			glog.Errorf("Unable to acquire health checks: %+v", err)
-			return
-		}
-		for iname, icheck := range healthChecks {
-			_, ok = instanceStatus[iname]
-			if !ok {
-				instanceStatus[iname] = &domain.HealthCheckStatus{name, "unknown", 0, icheck.Interval.Seconds(), time.Now().Unix(), ""}
-			}
-		}
-	}
-	thisStatus, ok = instanceStatus[name]
-	if !ok {
-		glog.Warningf("ignoring %s health status %s, not found in service %s", passed, name, serviceID)
-		return
-	}
-	thisStatus.Status = passed
-	thisStatus.Timestamp = time.Now().UTC().Unix()
 }
