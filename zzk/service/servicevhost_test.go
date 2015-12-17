0dfa5f847996bb72d6776b9cd6f363384746a4da
morr-zenoss <morr@zenoss.com>
2015-12-17 12:04:06 -0600

    add unit test

---
+++ zzk/service/servicevhost_test.go
@@ -0,0 +1,68 @@
+package service
+import (
+"testing"
+"github.com/control-center/serviced/coordinator/client/mocks"
+"github.com/control-center/serviced/domain/service"
+	"github.com/control-center/serviced/domain/servicedefinition"
+)
+// Tests:
+// Initial conditions:
+// - no servicevhosts node
+// - empty servicevhosts node
+// - servicevhosts node with other data
+// - servicevhosts node already populated
+// Inputs:
+// - service with no vhost - no change to initial conditions
+// - service with vhost - end state should have servicevhosts node populated with service entry
+
+type ServiceProfile struct {
+	Name string
+	VHostNames []string
+}
+
+func makeService(sp ServiceProfile) *service.Service {
+	svc := new (service.Service)
+	svc.Name = sp.Name
+	for _, vhn := range(sp.VHostNames) {
+		svc.Endpoints = append(
+			svc.Endpoints, service.ServiceEndpoint{
+				Name:vhn,
+				VHostList: []servicedefinition.VHost {
+					{
+						Name: vhn,
+						Enabled: true,
+					},
+				},
+			})
+	}
+	svc.Endpoints = []service.ServiceEndpoint {
+		{
+			VHostList: []servicedefinition.VHost{
+				{
+					Name: "test1",
+					Enabled: true,
+				},
+			},
+		},
+	}
+	return svc
+}
+
+func TestUpdateServiceVHosts(t *testing.T) {
+	testSvcProfile := ServiceProfile{
+		Name: "Test Service",
+		VHostNames: []string {"vhost1", "vhost2"},
+	}
+
+	mockConn := new(mocks.Connection)
+	mockSvc := makeService(testSvcProfile)
+
+	mockConn.On("Children",zkServiceVhosts).Return([]string{"foo", "bar"}, nil)
+	mockConn.On("Get",zkServiceVhosts + "/:vhOn:__test1",&ServiceVhostNode{"","",false,nil}).Return(nil)
+	mockConn.On("Set",zkServiceVhosts + "/:vhOn:__test1",&ServiceVhostNode{"","test1",true,nil}).Return(nil)
+
+	UpdateServiceVhosts(mockConn, mockSvc)
+
+	mockConn.AssertNumberOfCalls(t, "Children", 1)
+	mockConn.AssertNumberOfCalls(t, "Set", 1)
+}
