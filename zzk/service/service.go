e20282fecb8deee079ead1ca15374e4eae9cdfc6
Ken Walker <kwalker@zenoss.com>
2015-12-31 13:10:10 -0600

    Fixes CC-1697
    cherry-pick commit e8ca9f191d560c018aaf59d78e24b1ae46efb4b4
    (committed by "zendev port cherry-pick")

--- zzk/service/service.go
+++ zzk/service/service.go
@@ -403,6 +403,12 @@ func StopService(conn client.Connection, serviceID string) error {
 
 // SyncServices synchronizes all services into zookeeper
 func SyncServices(conn client.Connection, svcs []service.Service) error {
+	// Make sure service path exists (during upgrades, sometimes it disappears: CC-1691)
+	if err := conn.CreateDir(servicepath()); err != nil && err != client.ErrNodeExists {
+		glog.Errorf("Cannot create service path %s: %s", servicepath(), err)
+		return err
+	}
+
 	svcNodes, err := conn.Children(servicepath())
 	if err != nil {
 		glog.Errorf("Can not look up services: %s", err)
