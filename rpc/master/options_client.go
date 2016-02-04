428a772274b89d664488906cc194e1a44a3003ad
Rye Terrell <rterrell@zenoss.com>
2016-02-04 07:55:09 -0600

    adds options to rpc

---
+++ rpc/master/options_client.go
@@ -0,0 +1,24 @@
+// Copyright 2014 The Serviced Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package master
+
+import "github.com/control-center/serviced/cli"
+
+func (c *Client) GetOptions() (*cli.Options, error) {
+	response := cli.Options{}
+	if err := c.call("GetOptions", empty, &response); err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
