1fc2b079438c59488fcd7a0e00e2befb1729df8b
jplouis <jplouis@gmail.com>
2016-02-11 13:03:43 -0600

    Merge pull request #2495 from control-center/feature/CC-1916
    CC-1916: migrate the docker registry before starting the web server

--- cli/api/daemon.go
+++ cli/api/daemon.go
@@ -475,6 +475,11 @@ func (d *daemon) startMaster() (err error) {
 		return err
 	}
 
+	if err = d.facade.UpgradeRegistry(d.dsContext, "", false); err != nil {
+		glog.Errorf("Could not upgrade registry: %s", err)
+		return err
+	}
+
 	if err = d.registerMasterRPC(); err != nil {
 		glog.Errorf("Could not register master RPCs: %s", err)
 		return err
