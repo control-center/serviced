16fcfb2940c26219353e4890e5ff5ff4a8720b60
Glenn Jones <gjones@zenoss.com>
2016-02-17 10:16:29 -0600

    OSX Port: update glog version to on that works on OSX

--- Godeps/_workspace/src/github.com/zenoss/glog/glog.go
+++ Godeps/_workspace/src/github.com/zenoss/glog/glog.go
@@ -99,7 +99,7 @@ func isatty(f *os.File) bool {
 	}
 	var t syscall.Termios
 	_, _, errno := syscall.Syscall6(syscall.SYS_IOCTL,
-		f.Fd(), syscall.TCGETS,
+		f.Fd(), ioctlTermioFlag,
 		uintptr(unsafe.Pointer(&t)), 0, 0, 0)
 	return errno == 0
 }
