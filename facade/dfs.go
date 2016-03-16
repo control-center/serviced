96b1f31003371aedf4925caeb0b717a9ca2d97ab
John Hamilton <johnhamilton@zenoss.com>
2016-03-16 17:17:53 -0500

    Cleaned up some comments, type-os and log messages

--- facade/dfs.go
+++ facade/dfs.go
@@ -638,8 +638,7 @@ func (info *registryVersionInfo) start(isvcsRoot string, hostPort string) (*dock
 	return container, nil
 }
 
-// DockerOverride will replace a docker image in the registry with a new image for all services that use
-//  it under the given tenant
+// DockerOverride will replace a docker image in the registry with a new image
 func (f *Facade) DockerOverride(ctx datastore.Context, newImageName, oldImageName string) error {
 	return f.dfs.Override(newImageName, oldImageName)
 }
