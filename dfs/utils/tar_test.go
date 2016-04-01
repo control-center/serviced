b7b6ff6ecb36eef29989d00c46f72fe7e892a9b0
Ian McCracken <ian.mccracken@gmail.com>
2016-04-01 17:01:53 -0500

    Saving state

--- dfs/utils/tar_test.go
+++ dfs/utils/tar_test.go
@@ -16,14 +16,16 @@
 package utils_test
 
 import (
-	"archive/tar"
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/control-center/serviced/dfs/utils"
 	. "gopkg.in/check.v1"
+	"gopkg.in/pipe.v2"
 )
 
 type TarTestSuite struct{}
@@ -42,7 +44,7 @@ func TestTar(t *testing.T) { TestingT(t) }
 
 func tarfile(dir, size string, numfiles int) string {
 	i += 1
-	tardir := fmt.Sprintf("%s/test-%d", dir, i)
+	tardir := filepath.Join(dir, fmt.Sprintf("test-%d", i))
 	os.Mkdir(tardir, os.ModePerm)
 	for j := 0; j < numfiles; j++ {
 		fname := getFileName(tardir)
@@ -50,37 +52,25 @@ func tarfile(dir, size string, numfiles int) string {
 		exec.Command("/bin/bash", "-c", cmd).Run()
 	}
 	tar := fmt.Sprintf("%s.tar", tardir)
-	exec.Command("tar", "cf", tar, tardir).Run()
+	exec.Command("tar", "-C", tardir, "-cf", tar, ".").Run()
 	return tar
 }
 
-func countFilesInTar(fname string) int {
-	tarfile, _ := os.Open(fname)
-	defer tarfile.Close()
-	reader := tar.NewReader(tarfile)
-	var j int
-	for {
-		if _, err := reader.Next(); err != nil {
-			fmt.Println(err)
-			break
-		}
-		j += 1
-	}
-	return j
-}
-
-func (s *TarTestSuite) TestSingleTar(c *C) {
+func (s *TarTestSuite) TestPrefixPath(c *C) {
 	dir := c.MkDir()
 	testfile := tarfile(dir, "100KB", 2)
-	instream, _ := os.Open(testfile)
-	outfile := fmt.Sprintf("%s/out.tar", dir)
-	outstream, _ := os.Create(outfile)
-
-	m := NewTarStreamMerger(outstream)
-	m.Append(instream)
-	m.Close()
-	outstream.Close()
-
-	c.Assert(countFilesInTar(outfile), Equals, 2)
-
+	outfile := filepath.Join(dir, "out.tar")
+	p := pipe.Line(
+		pipe.ReadFile(testfile),
+		PrefixPath("prefix/", dir),
+		pipe.WriteFile(outfile, 0644),
+	)
+	stdout, err := pipe.CombinedOutput(p)
+	c.Log(string(stdout))
+	c.Assert(err, IsNil)
+	outdir := filepath.Join(dir, "testout")
+	os.Mkdir(outdir, os.ModePerm)
+	exec.Command("tar", "-C", outdir, "-xf", outfile).Run()
+	os.ListDir(outdir)
+	time.Sleep(30 * time.Second)
 }
