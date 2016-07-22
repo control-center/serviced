33bcd9040ee69cea96992290f4a03feacd498817
Young Lee <ylee@zenoss.com>
2016-07-22 14:39:54 -0500

    Fix image tag

--- isvcs/isvc.go
+++ isvcs/isvc.go
@@ -25,7 +25,7 @@ var Mgr *Manager
 
 const (
 	IMAGE_REPO    = "zenoss/serviced-isvcs"
-	IMAGE_TAG     = "v44-dev"
+	IMAGE_TAG     = "v45"
 	ZK_IMAGE_REPO = "zenoss/isvcs-zookeeper"
 	ZK_IMAGE_TAG  = "v4"
 )
