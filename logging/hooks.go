9941ef302c6eef0fae952177d724d5ab6448769e
Ian McCracken <ian.mccracken@gmail.com>
2016-08-22 10:46:28 -0500

    Added some comments

--- logging/hooks.go
+++ logging/hooks.go
@@ -14,12 +14,16 @@ const (
 	vendorprefix = prefix + "vendor/"
 )
 
+// ContextHook is a hook to provide context in log messages
 type ContextHook struct{}
 
+// Levels satisfies the logrus.Hook interface. This hook applies to all levels.
 func (hook ContextHook) Levels() []logrus.Level {
 	return logrus.AllLevels
 }
 
+// Fire satisfies the logrus.Hook interface. This impl figures out the file and
+// line number of the caller and adds them to the data.
 func (hook ContextHook) Fire(entry *logrus.Entry) error {
 	pc := make([]uintptr, 3, 3)
 	count := runtime.Callers(6, pc)
