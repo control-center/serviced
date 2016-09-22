c8c629e5de693a9e960cddd0fd67455956555ba1
Ron Phillips <rphillips@zenoss.com>
2016-09-22 17:06:02 -0500

    Make GetRemoteConnection testable

--- web/utils.go
+++ web/utils.go
@@ -73,24 +73,57 @@ func GetCertFiles(certFile, keyFile string) (string, string) {
 	return certFile, keyFile
 }
 
+// Dialer interface to make getRemoteConnection testable.
+type dialerInterface interface {
+	Dial(string, string) (net.Conn, error)
+}
+
+type netDialer struct{}
+
+func (d *netDialer) Dial(network, address string) (net.Conn, error) {
+	return net.Dial(network, address)
+}
+
+func newNetDialer() dialerInterface {
+	return &netDialer{}
+}
+
+type tlsDialer struct{
+	config *tls.Config
+}
+
+func (d *tlsDialer) Dial(network, address string) (net.Conn, error) {
+	return tls.Dial(network, address, d.config)
+}
+
+func newTlsDialer(config *tls.Config) dialerInterface {
+	return &tlsDialer{config: config}
+}
+
 // GetRemoteConnection returns a connection to a remote address
 func GetRemoteConnection(useTLS bool, export *registry.ExportDetails) (remote net.Conn, err error) {
-	isLocalAddress := IsLocalAddress(export.HostIP)
+	var dialer dialerInterface
+	if useTLS {
+		config := tls.Config{InsecureSkipVerify: true}
+		dialer = newTlsDialer(&config)
+	} else {
+		dialer = newNetDialer()
+	}
+	return getRemoteConnection(export, dialer)
+}
 
+func getRemoteConnection(export *registry.ExportDetails, dialer dialerInterface) (remote net.Conn, err error) {
+	isLocalAddress := IsLocalAddress(export.HostIP)
+	
 	if isLocalAddress {
 		// if the address is local return the connection
 		address := fmt.Sprintf("%s:%d", export.PrivateIP, export.PortNumber)
-		return net.Dial("tcp4", address)
+		return dialer.Dial("tcp4", address)
 	}
 
 	// set up the remote address
 	remoteAddress := fmt.Sprintf("%s:%d", export.HostIP, export.MuxPort)
-	if useTLS {
-		config := tls.Config{InsecureSkipVerify: true}
-		remote, err = tls.Dial("tcp4", remoteAddress, &config)
-	} else {
-		remote, err = net.Dial("tcp4", remoteAddress)
-	}
+	remote, err = dialer.Dial("tcp4", remoteAddress)
 
 	// Prevent a panic if we couldn't connect to the mux.
 	if err != nil {
