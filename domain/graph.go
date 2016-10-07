7258e16a31c334999bf3498ca84987cdc7f126f8
Summer Mousa <smousa@zenoss.com>
2016-10-07 08:44:22 -0500

    added validations when updating monitoring profile and updated the internal metrics code

--- domain/graph.go
+++ domain/graph.go
@@ -16,7 +16,9 @@ package domain
 
 import (
 	"encoding/json"
+	"fmt"
 	"reflect"
+	"strings"
 )
 
 //DataPointRateOptions define the rate options for a data point
@@ -124,4 +126,12 @@ type GraphConfig struct {
 // Equals returns if graph equals that graph
 func (graph *GraphConfig) Equals(that *GraphConfig) bool {
 	return reflect.DeepEqual(graph, that)
+}
+
+// ValidEntity ensures that no graph configs have an id with prefix "internal"
+func (graph GraphConfig) ValidEntity() error {
+	if strings.HasPrefix(graph.ID, "internal") {
+		return fmt.Errorf("graph id cannot have prefix 'internal'")
+	}
+	return nil
 }
