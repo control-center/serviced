40213c32d455341aa56e1bc9ddc14c848c1ae7dc
Izzy <marevalo@zenoss.com>
2016-10-19 17:10:54 -0500

    Initial commit

--- tools/serviced-storage/driver.go
+++ tools/serviced-storage/driver.go
@@ -14,11 +14,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/user"
 	"path/filepath"
+	"strings"
 
 	"github.com/control-center/serviced/volume"
 	"github.com/jessevdk/go-flags"
@@ -42,7 +44,11 @@ type DriverSet struct {
 }
 
 // DriverUnset is the subcommand for unsetting the default driver
-type DriverUnset struct{}
+type DriverUnset struct {
+	Args struct {
+		Remove string `description:"Use this flag to automatically remove orphaned devices"`
+	}
+}
 
 // DriverDisable is the subcommand to disable the driver
 type DriverDisable struct {
@@ -63,6 +69,14 @@ type DriverList struct {
 	Args struct {
 		Path flags.Filename `description:"Path of the driver"`
 	} `positional-args:"yes" optional:"yes"`
+}
+
+// CheckOrphans is the subcommand for displaying orphaned volumes
+type CheckOrphans struct {
+	Clean bool `description:"Indicates that the orphans should be removed" long:"clean" short:"c"`
+	Args  struct {
+		Path flags.Filename `long:"driver" short:"d" description:"Path of the driver"`
+	} `positional-args:"yes"`
 }
 
 // Execute initializes a new storage driver
@@ -172,6 +186,118 @@ func (c *DriverList) Execute(args []string) error {
 	}
 	for _, volname := range driver.List() {
 		fmt.Println(volname)
+	}
+	return nil
+}
+
+// Execute displays orphaned volumes
+func (c *CheckOrphans) Execute(args []string) error {
+	App.initializeLogging()
+	type Metadata struct {
+		CurrentDevice string
+		Snapshots     interface{}
+	}
+
+	directory := GetDefaultDriver(string(c.Args.Path))
+	volDir := directory + "/.devicemapper/volumes"
+	fsVolDir := directory + "/.devicemapper/metadata"
+
+	files, err := ioutil.ReadDir(volDir)
+	if err != nil {
+		return err
+	}
+
+	var ccVols []string
+	var snaps []string
+	var orphans []string
+	for _, f := range files {
+		if strings.Contains(f.Name(), ".") {
+			snaps = append(snaps, f.Name())
+			orphans = append(orphans, f.Name())
+		} else {
+			ccVols = append(ccVols, f.Name())
+		}
+	}
+
+	// This will store CC-visible device identifiers for comparing with the filesystem's metadata
+	var known []string
+	for _, v := range ccVols {
+		dat, _ := ioutil.ReadFile(volDir + "/" + v + "/metadata.json")
+		var m Metadata
+		json.Unmarshal(dat, &m)
+		known = append(known, m.CurrentDevice)
+		for k, vv := range m.Snapshots.(map[string]interface{}) {
+			known = append(known, vv.(string))
+			// Check if this snapshot is in our metadata
+			for i, s := range orphans {
+				if s == k {
+					orphans = append(orphans[:i], orphans[i+1:]...)
+				}
+			}
+		}
+	}
+	if len(orphans) > 0 {
+		fmt.Println("Orphans were found:")
+		for _, o := range orphans {
+			fmt.Println("-", o)
+		}
+	} else {
+		fmt.Println("No orphaned device volumes found")
+		return nil
+	}
+
+	// now we compare these against the devices on the filesystem metadata to divine the needed "device_id"
+	filez, err := ioutil.ReadDir(fsVolDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// create a clean map of filesystem IDs, each mapped to their respective hash IDs
+	var fsVols map[string]float64
+	fsVols = map[string]float64{}
+	for _, f := range filez {
+		d := fsVolDir + "/" + f.Name()
+		dat, err := ioutil.ReadFile(d)
+		if err != nil {
+			return err
+		}
+		var m map[string]interface{}
+		json.Unmarshal(dat, &m)
+		for k, v := range m {
+			if k == "initialized" && v.(bool) == false {
+				fsVols[f.Name()] = m["device_id"].(float64)
+			}
+		}
+	}
+
+	// store only the device ids of the devices we care about
+	var zorphans map[string]float64
+	zorphans = map[string]float64{}
+	for k, v := range fsVols {
+		found := false
+		for _, vv := range known {
+			if k == vv {
+				found = true
+			}
+		}
+		if found == false {
+			zorphans[k] = v
+		}
+	}
+
+	if len(zorphans) > 0 {
+		for k, v := range zorphans {
+			fmt.Println(k, v)
+			if c.Clean {
+				driver, err := InitDriverIfExists(directory)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if err := driver.Remove(k); err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println("Removed orphaned snapshot", k)
+			}
+		}
 	}
 	return nil
 }
