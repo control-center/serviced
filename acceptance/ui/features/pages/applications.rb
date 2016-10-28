1b31c64433323151649af134da72611fd1ac8508
Glenn Jones <gjones@zenoss.com>
2016-10-28 09:40:07 -0500

    Remove unused references to services map

--- acceptance/ui/features/pages/applications.rb
+++ acceptance/ui/features/pages/applications.rb
@@ -9,7 +9,6 @@ class Applications < SitePrism::Page
 
     element :addApp_button, "[ng-click='modal_deployWizard()']"
     element :addAppTemplate_button, "[ng-click='modal_addTemplate()']"
-    element :servicesMap_button, "a[href='/#/servicesmap'][class='btn-link']"
     element :deploymentID_field, "input[name='deploymentID']"
     element :services_table, "table[data-config='servicesTable']"
     element :templates_table, "table[data-config='templatesTable']"
