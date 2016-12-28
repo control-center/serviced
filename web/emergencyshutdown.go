c0210fdaf3bdb6683ebccc81f2b337db72eb2358
Sean Pope <spope@zenoss.com>
2016-12-28 11:25:30 -0600

    Use dao req.

--- web/emergencyshutdown.go
+++ web/emergencyshutdown.go
@@ -1,6 +1,9 @@
 package web
 
-import rest "github.com/zenoss/go-json-rest"
+import (
+	"github.com/control-center/serviced/dao"
+	rest "github.com/zenoss/go-json-rest"
+)
 
 type EmergencyShutdownRequest struct {
 	Operation int // 0 is emergency shutdown, 1 is clear emergency shutdown status
@@ -15,10 +18,20 @@ func restEmergencyShutdown(w *rest.ResponseWriter, r *rest.Request, ctx *request
 		restBadRequest(w, err)
 		return
 	}
-	err = ctx.getFacade().EmergencyShutdownRequest(req.TenantID, req.Operation)
+	stateID := "stop"
+	if req.Operation != 0 {
+		stateID = "go"
+	}
+	daoReq := dao.ServiceStateRequest{
+		ServiceID:      req.TenantID,
+		ServiceStateID: stateID,
+	}
+	n, err := ctx.getFacade().EmergencyStopService(ctx, daoReq)
 	if err != nil {
 		plog.WithError(err).Error("Facade could not process Emergency Shutdown Request")
 		restBadRequest(w, err)
+		return
 	}
+	plog.Infof("Scheduled %d services", n)
 	restSuccess(w)
 }
