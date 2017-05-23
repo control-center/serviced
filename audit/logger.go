45c149099a6a06735ba2fc3638cf2b44fa9bf7a1
Cornelius Toole <ctoole@zenoss.com>
2017-05-23 17:19:29 -0500

    cleanup wrt DRY principle, create new obj, then add fields

--- audit/logger.go
+++ audit/logger.go
@@ -81,8 +81,9 @@ func (l *logger) Action(action string) Logger {
 }
 
 func (l *logger) Message(ctx datastore.Context, message string) Logger {
-	l.message = message
-	return l.newLoggerWith("user", ctx.User())
+	result := l.newLoggerWith("user", ctx.User())
+	result.message = message
+	return result
 }
 
 func (l *logger) Type(theType string) Logger {
@@ -94,21 +95,28 @@ func (l *logger) ID(id string) Logger {
 }
 
 func (l *logger) Entity(entity datastore.Entity) Logger {
-	l.addField("id", entity.GetID())
-	return l.newLoggerWith("type", entity.GetType())
+	return l.newLoggerWithFields(logrus.Fields{
+		"id": entity.GetID(),
+		"type": entity.GetType(),
+	})
 }
 
 func (l *logger) WithField(name string, value string) Logger {
 	return l.newLoggerWith(name, value)
 }
 
-func (l *logger) newLoggerWith(name string, value string) Logger {
-	l.addField(name, value)
-	return &logger{
-		entry:   l.entry,
+func (l *logger) newLoggerWith(name string, value string) *logger {
+	return l.newLoggerWithFields(logrus.Fields{name: value})
+}
+
+func (l *logger) newLoggerWithFields(fields logrus.Fields) *logger {
+	result := &logger{
+		entry: l.entry,
 		message: l.message,
 		loggeri: l.loggeri,
 	}
+	result.addFields(fields)
+	return result
 }
 
 func (l *logger) addField(name string, value string) Logger {
@@ -116,7 +124,7 @@ func (l *logger) addField(name string, value string) Logger {
 }
 
 func (l *logger) WithFields(fields logrus.Fields) Logger {
-	return l.addFields(fields)
+	return l.newLoggerWithFields(fields)
 }
 
 func (l *logger) addFields(fields logrus.Fields) Logger {
