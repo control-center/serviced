719bf7baad42e5d34bb1b6c4debdb03662896642
ssOlexBaiko <obaiko@softserveinc.com>
2017-08-28 13:30:11 +0000

    add integration test

--- facade/service_test.go
+++ facade/service_test.go
@@ -3770,3 +3770,9 @@ func (ft *FacadeIntegrationTest) assertPathResolvesToServices(c *C, path string,
 	c.Assert(foundids, DeepEquals, ids)
 }
 
+func (ft *FacadeIntegrationTest) TestFacade_getChanges(c *C) {
+	beforeUpdates := ""
+	updates := ft.Facade.getChanges(ft.CTX, service.Service{ID: "updatedID"})
+	c.Assert(beforeUpdates, Equals, updates)
+}
+
