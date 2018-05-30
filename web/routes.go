b1250357f9b7536080e5133a1c43a3e851ecc64d
Mike Orr <morr@zenoss.com>
2018-05-30 15:21:34 -0500

    fix FF failure and revert file inadvertently checked in by JP.

--- web/routes.go
+++ web/routes.go
@@ -111,7 +111,7 @@ func (sc *ServiceConfig) getRoutes() []rest.Route {
 
 		// Generic static data
 		rest.Route{"GET", "/favicon.ico", gz(favIcon)},
-		rest.Route{"GET", "/static/globals.js", gz(sc.noAuth(restGetAuth0Config))},
+		rest.Route{"GET", "/static/globals.js", sc.noAuth(restGetAuth0Config)},
 		rest.Route{"GET", "/static/*resource", gz(staticData)},
 		rest.Route{"GET", "/licenses.html", gz(licenses)},
 
