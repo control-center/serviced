8039b39b5ae3b69515e1b03d256c673f295ac2b6
Eric Cifreo <ecifreo@zenoss.com>
2018-07-11 15:55:18 -0500

    First pass at serviced promote jenkins job

---
+++ pkg/jenkins/promote.groovy
@@ -0,0 +1,54 @@
+#!groovy
+//
+// promote.groovy - used in Jenkins to promote a serviced build from one maturity to the next.
+//
+pipeline {
+
+    agent {
+        dockerfile {
+            dir 'pkg/reversion/deb'
+        }
+    }
+
+    parameters {
+        choice(name: 'SOURCE_MATURITY', choices: 'unstable\ntesting', description: 'The maturity for packages to be promoted.')
+        string(name: 'SOURCE_VERSION', description: 'If looking for unstable, provide the build number of the desired \'merge-start\' serviced build.')
+        choice(name: 'TARGET_MATURITY', choices: 'testing\nstable', description: 'The maturity for promoted packages.')
+        string(name: 'TARGET_VERSION', description: '')
+    }
+
+    stages {
+        stage('Fetch source artifact') {
+            steps {
+                googleStorageDownload(credentialsId: 'zing-registry-188222', bucket: 'gs://cz-${SOURCE_MATURITY}/serviced/${SOURCE_VERSION}/*.deb')
+            }
+        }
+
+        stage('Repackage') {
+            steps {
+                sh """
+                    rm -rf output
+                    ls -la
+                    dpkg-deb -f *.deb
+                    FILE=`ls *.deb`
+                    mkdir -p output && cd output
+                    echo -e "\\nMetadata for the source package"
+                    dpkg -f "${FILE}"
+                    
+                    # run reversion
+                    dpkg-reversion -b -v ${TARGET_VERSION} "${FILE}"
+
+                    echo -e "\\nMetadata for the source package"
+                    serviced_${TARGET_VERSION}_amd64.deb 
+                """
+            }
+        }
+
+        stage('Promote artifact') {
+            steps {
+                googleStorageUpload(credentialsId: 'zing-registry-188222', bucket: 'gs://cz-${TARGET_MATURITY}/serviced', pattern:'output/*.deb', pathPrefix: 'output')
+            }
+        }
+    }
+
+}
