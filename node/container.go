dde7ac513b6b708d37b5f65ddf9a87fdfc29c19c
Oleksandr Styhar <ostyhar@zenoss.com>
2019-10-01 13:48:33 +0000

    CC-4259: Revert SERVICE and PARENT env variables.

--- node/container.go
+++ node/container.go
@@ -541,6 +541,7 @@ func (a *HostAgent) getService(serviceID string) (*service.Service, error) {
 	return svc, nil
 }
 
+
 // dockerLogsToFile dumps container logs to file
 func dockerLogsToFile(containerid string, numlines int) {
 	// TODO: need to get logs from api
@@ -594,25 +595,6 @@ func (a *HostAgent) setupContainer(tenantID string, svc *service.Service, instan
 	state.ContainerID = ctr.ID
 
 	return ctr, state, nil
-}
-
-func (a *HostAgent) getParentName(svc *service.Service) string {
-	logger := plog.WithFields(log.Fields{
-		"servicename": svc.Name,
-		"serviceid":   svc.ID,
-	})
-
-	parentID := svc.ParentServiceID
-	if parentID == "" {
-		return ""
-	}
-	parent, err := a.getService(parentID)
-	if err != nil {
-		logger.WithError(err).Debug("Unable to read parent service %s", parentID)
-		return ""
-	}
-
-	return parent.Name
 }
 
 func (a *HostAgent) createContainerConfig(tenantID string, svc *service.Service, instanceID int, imageUUID string) (*dockerclient.Config, *dockerclient.HostConfig, *zkservice.ServiceState, error) {
@@ -813,14 +795,6 @@ func (a *HostAgent) createContainerConfig(tenantID string, svc *service.Service,
 		fmt.Sprintf("DOCKER_14203_FIX='%s'", fix),
 		// End temp fix part 2. See immediately above for part 1.
 	)
-
-	parent := a.getParentName(svc)
-	if parent != "" {
-		cfg.Env = append(svc.Environment,
-			fmt.Sprintf("SERVICE=%s", svc.Name),
-			fmt.Sprintf("PARENT=%s", parent),
-		)
-	}
 
 	// add dns values to setup
 	for _, addr := range a.dockerDNS {
