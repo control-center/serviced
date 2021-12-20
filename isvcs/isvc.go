26f1cb101f0609e5ae0f6478e773b9c472158e41
deer-warlord <deerwarlord@gmail.com>
2021-12-20 12:52:13 +0200

    CC-4476: ElasticSearch-serviced - Log4j compatibility

--- isvcs/isvc.go
+++ isvcs/isvc.go
@@ -35,9 +35,9 @@ var (
 
 const (
 	IMAGE_REPO         = "zenoss/serviced-isvcs"
-	IMAGE_TAG          = "v69"
+	IMAGE_TAG          = "v70"
 	ZK_IMAGE_REPO      = "zenoss/isvcs-zookeeper"
-	ZK_IMAGE_TAG       = "v15"
+	ZK_IMAGE_TAG       = "v14"
 	OTSDB_BT_REPO      = "zenoss/isvcs-metrics-bigtable"
 	OTSDB_BT_TAG       = "v1"
 	API_KEY_PROXY_REPO = "gcr.io/zing-registry-188222/api-key-proxy"
